a013783c1ef00fd66526e3ad361fc6b523114984
Sean Feldman <feldman.sean@gmail.com>
2019-09-10 18:54:55 -0600

    Added Saga ID generator

--- src/NServiceBus.Persistence.CosmosDB/Saga/CosmosDbSagaPersistence.cs
+++ src/NServiceBus.Persistence.CosmosDB/Saga/CosmosDbSagaPersistence.cs
@@ -1,12 +1,14 @@
 ﻿namespace NServiceBus.Features
 {
     using NServiceBus.Sagas;
+    using Persistence.CosmosDB;
 
     class CosmosDbSagaPersistence : Feature
     {
         internal CosmosDbSagaPersistence()
         {
             DependsOn<Sagas>();
+            Defaults(s => s.SetDefault<ISagaIdGenerator>(new SagaIdGenerator()));
         }
 
         protected override void Setup(FeatureConfigurationContext context)
---
+++ src/NServiceBus.Persistence.CosmosDB/Saga/SagaIdGenerator.cs
@@ -0,0 +1,39 @@
+﻿namespace NServiceBus.Persistence.CosmosDB
+{
+    using System;
+    using System.Security.Cryptography;
+    using System.Text;
+    using Sagas;
+
+    class SagaIdGenerator : ISagaIdGenerator
+    {
+        public Guid Generate(SagaIdGeneratorContext context)
+        {
+            if (context.CorrelationProperty == SagaCorrelationProperty.None)
+            {
+                throw new Exception("The CosmosDB saga persister doesn't support custom saga finders.");
+            }
+
+            return Generate(context.SagaMetadata.SagaEntityType, context.CorrelationProperty.Value);
+        }
+
+        static Guid Generate(Type sagaEntityType, object correlationPropertyValue)
+        {
+            // assumes single correlated sagas since v6 doesn't allow more than one corr prop
+            // will still have to use a GUID since moving to a string id will have to wait since its a breaking change
+            return DeterministicGuid($"{sagaEntityType}_{correlationPropertyValue}");
+        }
+
+        static Guid DeterministicGuid(string src)
+        {
+            var stringBytes = Encoding.UTF8.GetBytes(src);
+
+            using (var sha1CryptoServiceProvider = new SHA1CryptoServiceProvider())
+            {
+                var hashedBytes = sha1CryptoServiceProvider.ComputeHash(stringBytes);
+                Array.Resize(ref hashedBytes, 16);
+                return new Guid(hashedBytes);
+            }
+        }
+    }
+}
