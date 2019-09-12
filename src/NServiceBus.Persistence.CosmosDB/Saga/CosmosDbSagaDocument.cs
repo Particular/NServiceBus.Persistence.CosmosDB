730105c126f80f6a6ff3f8f2069c8965488756ba
Sean Feldman <feldman.sean@gmail.com>
2019-09-12 10:03:12 -0600

    Map properties to cosmos field names
    Co-authored-by: Andreas Ohlund <andreasohlund2@gmail.com>
    Co-authored-by: Sean Feldman <feldman.sean@gmail.com>

--- src/NServiceBus.Persistence.CosmosDB/Saga/CosmosDbSagaDocument.cs
+++ src/NServiceBus.Persistence.CosmosDB/Saga/CosmosDbSagaDocument.cs
@@ -1,11 +1,14 @@
 ﻿namespace NServiceBus.Persistence.CosmosDB
 {
     using System;
+    using Newtonsoft.Json;
 
     class CosmosDbSagaDocument
     {
+        [JsonProperty(PropertyName = "partitionKey")]
         public string PartitionKey { get; set; }
 
+        [JsonProperty(PropertyName = "id")]
         public Guid SagaId { get; set; }
 
         public string EntityType { get; set; } = "Saga";
