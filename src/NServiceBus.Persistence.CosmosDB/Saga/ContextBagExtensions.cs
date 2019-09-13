1128a4fe4e5452380d80ad8e10fe478db3403b90
Sean Feldman <feldman.sean@gmail.com>
2019-09-12 21:48:12 -0600

    Extension method to extract Saga type from the context bag

---
+++ src/NServiceBus.Persistence.CosmosDB/Saga/ContextBagExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace NServiceBus.Persistence.CosmosDB
+{
+    using System;
+    using Extensibility;
+    using Sagas;
+
+    static class ContextBagExtensions
+    {
+        public static Type GetSagaType(this ContextBag contextBag)
+        {
+            var activeSagaInstance = contextBag.Get<ActiveSagaInstance>();
+            var sagaType = activeSagaInstance.Instance.GetType();
+
+            return sagaType;
+        }
+    }
+}
