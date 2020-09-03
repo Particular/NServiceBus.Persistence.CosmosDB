27b35ee34f808e0373dfbd7532d64b896145a2fd
Bob Langley <bob.langley@particular.net>
2020-09-03 15:53:34 -0700

    Add OutboxPersister
    Co-authored-by: Sean Feldman <feldman.sean@gmail.com>

---
+++ src/NServiceBus.Persistence.CosmosDB/Outbox/OutboxPersister.cs
@@ -0,0 +1,44 @@
+﻿namespace NServiceBus.Persistence.CosmosDB.Outbox
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+    using NServiceBus.Extensibility;
+    using NServiceBus.Outbox;
+
+    class OutboxPersister : IOutboxStorage
+    {
+        public Task<OutboxTransaction> BeginTransaction(ContextBag context)
+        {
+            return Task.FromResult((OutboxTransaction)new CosmosOutboxTransaction());
+        }
+
+        public Task<OutboxMessage> Get(string messageId, ContextBag context)
+        {
+            //This always must return null for the Outbox "hack" to work
+            return null;
+        }
+
+        public Task Store(OutboxMessage message, OutboxTransaction transaction, ContextBag context)
+        {
+            var cosmosTransaction = transaction as CosmosOutboxTransaction;
+
+            if (cosmosTransaction.TransactionalBatch is null)
+            {
+                return Task.CompletedTask;
+            }
+
+            //TODO: Probably serialize and add keypath/partitionkey JTokens and id
+
+            cosmosTransaction.TransactionalBatch.CreateItem(new OutboxRecord { Id = message.MessageId, TransportOperations = message.TransportOperations });
+
+            return Task.CompletedTask;
+        }
+
+        public Task SetAsDispatched(string messageId, ContextBag context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
