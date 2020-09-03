238edf5a094dfa9b598feb84715ce301e01ddc63
Bob Langley <bob.langley@particular.net>
2020-09-03 15:53:50 -0700

    Add CosmosOutboxTransaction
    Co-authored-by: Sean Feldman <feldman.sean@gmail.com>

---
+++ src/NServiceBus.Persistence.CosmosDB/Outbox/CosmosOutboxTransaction.cs
@@ -0,0 +1,28 @@
+﻿namespace NServiceBus.Persistence.CosmosDB.Outbox
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading.Tasks;
+    using NServiceBus.Outbox;
+
+    class CosmosOutboxTransaction : OutboxTransaction
+    {
+        internal TransactionalBatchDecorator TransactionalBatch { get; set; }
+
+        public Task Commit()
+        {
+            if (TransactionalBatch is null)
+            {
+                return Task.CompletedTask;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
