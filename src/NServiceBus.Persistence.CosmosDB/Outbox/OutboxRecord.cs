e3c20fcb1d36fc456ec97d1f596ed5f34da057a9
Bob Langley <bob.langley@particular.net>
2020-09-03 15:52:16 -0700

    Add OutboxRecord
    Co-authored-by: Sean Feldman <feldman.sean@gmail.com>

---
+++ src/NServiceBus.Persistence.CosmosDB/Outbox/OutboxRecord.cs
@@ -0,0 +1,14 @@
+﻿namespace NServiceBus.Persistence.CosmosDB.Outbox
+{
+    using System;
+    using NServiceBus.Outbox;
+
+    class OutboxRecord
+    {
+        public string Id { get; set; }
+
+        public DateTime? Dispatched { get; set; }
+
+        public TransportOperation[] TransportOperations { get; set; }
+    }
+}
