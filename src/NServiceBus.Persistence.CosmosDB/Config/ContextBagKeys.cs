686e794444417f52de3334e2efaa770fcb7326de
Bob Langley <bob.langley@particular.net>
2020-09-03 18:17:00 -0700

    Move ContextBagKeys to separate file

---
+++ src/NServiceBus.Persistence.CosmosDB/Config/ContextBagKeys.cs
@@ -0,0 +1,10 @@
+﻿namespace NServiceBus.Persistence.CosmosDB
+{
+    static class ContextBagKeys
+    {
+        const string baseName = "CosmosDB.";
+        public const string PartitionKeyValue = baseName + nameof(PartitionKeyValue);
+        public const string PartitionKeyPath = baseName + nameof(PartitionKeyPath);
+        public const string LogicalMessageId = baseName + nameof(LogicalMessageId);
+    }
+}
--- src/NServiceBus.Persistence.CosmosDB/Config/SettingsKeys.cs
+++ src/NServiceBus.Persistence.CosmosDB/Config/SettingsKeys.cs
@@ -12,11 +12,4 @@
             public const string JsonSerializerSettings =  nameof(baseName) + nameof(Sagas) + "." + nameof(JsonSerializerSettings);
         }
     }
-
-    static class ContextBagKeys
-    {
-        const string baseName = "CosmosDB.";
-        public const string PartitionKeyValue = baseName + nameof(PartitionKeyValue);
-        public const string PartitionKeyPath = baseName + nameof(PartitionKeyPath);
-    }
 }
