beb526260d6730234cc78024da0a4c6f521040d7
danielmarbach <daniel.marbach@openplace.net>
2020-09-04 16:04:34 +0200

    Comment out the initializer for now

--- src/NServiceBus.Persistence.CosmosDB/Saga/CosmosDbSagaPersistence.cs
+++ src/NServiceBus.Persistence.CosmosDB/Saga/CosmosDbSagaPersistence.cs
@@ -1,10 +1,11 @@
-﻿namespace NServiceBus.Features
+﻿namespace NServiceBus.Persistence.CosmosDB
 {
     using System.Threading.Tasks;
+    using Features;
     using Microsoft.Azure.Cosmos;
     using Newtonsoft.Json;
-    using NServiceBus.Sagas;
-    using Persistence.CosmosDB;
+    using Sagas;
+    using CosmosDB;
 
     class CosmosDbSagaPersistence : Feature
     {
@@ -20,13 +21,13 @@
 
         protected override void Setup(FeatureConfigurationContext context)
         {
-            var cosmosClient = context.Settings.Get<CosmosClient>(SettingsKeys.CosmosClient);
-            var databaseName = context.Settings.Get<string>(SettingsKeys.DatabaseName);
+            // var cosmosClient = context.Settings.Get<CosmosClient>(SettingsKeys.CosmosClient);
+            // var databaseName = context.Settings.Get<string>(SettingsKeys.DatabaseName);
             var serializerSettings = context.Settings.Get<JsonSerializerSettings>(SettingsKeys.Sagas.JsonSerializerSettings);
-            var sagaMetadataCollection = context.Settings.Get<SagaMetadataCollection>();
-            var partitionAwareConfiguration = context.Settings.Get<PartitionAwareConfiguration>();
+            // var sagaMetadataCollection = context.Settings.Get<SagaMetadataCollection>();
+            // var partitionAwareConfiguration = context.Settings.Get<PartitionAwareConfiguration>();
 
-            context.RegisterStartupTask(new InitializeContainers(cosmosClient, databaseName, sagaMetadataCollection, partitionAwareConfiguration));
+            //context.RegisterStartupTask(new InitializeContainers(cosmosClient, databaseName, sagaMetadataCollection, partitionAwareConfiguration));
 
             context.Container.ConfigureComponent(builder => new SagaPersister(serializerSettings), DependencyLifecycle.SingleInstance);
         }
