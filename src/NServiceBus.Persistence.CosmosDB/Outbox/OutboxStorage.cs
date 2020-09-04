e72b6827ad96f2767a0050ca8187d4e20ba58194
Bob Langley <bob.langley@particular.net>
2020-09-03 18:19:14 -0700

    Add OutboxStorage feature
    Co-authored-by: Sean Feldman <feldman.sean@gmail.com>

---
+++ src/NServiceBus.Persistence.CosmosDB/Outbox/OutboxStorage.cs
@@ -0,0 +1,40 @@
+﻿namespace NServiceBus.Persistence.CosmosDB.Outbox
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Microsoft.Azure.Cosmos;
+    using NServiceBus.Features;
+
+    class OutboxStorage : Feature
+    {
+        OutboxStorage()
+        {
+            Defaults(s => s.EnableFeatureByDefault<SynchronizedStorage>());
+            DependsOn<Features.Outbox>();
+        }
+
+        protected override void Setup(FeatureConfigurationContext context)
+        {
+            var client = context.Settings.Get<CosmosClient>(SettingsKeys.CosmosClient);
+
+            if (client is null)
+            {
+                throw new Exception("You must configure a CosmosClient or provide a connection string");
+            }
+
+            var partitionConfig = context.Settings.Get<PartitionAwareConfiguration>();
+
+            if (partitionConfig is null)
+            {
+                throw new Exception("No message partition mappings were found. Use persistence.Partition() to configure mappings.");
+            }
+
+            var databaseName = context.Settings.Get<string>(SettingsKeys.DatabaseName);
+
+            context.Container.ConfigureComponent(() => new OutboxPersister(), DependencyLifecycle.SingleInstance);
+
+            context.Pipeline.Register(new PartitioningBehavior(databaseName, client, partitionConfig), "Partition Behavior");
+        }
+    }
+}
