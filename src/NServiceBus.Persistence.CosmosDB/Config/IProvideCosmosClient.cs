acf139c283cb85d1369773d69bf47fe8920826b1
Bob Langley <bob.langley@particular.net>
2020-09-15 15:18:08 -0700

    Build client using DI and provide a default ThrowIfNoCosmosClientIsProvided registration
    Co-authored-by: Sean Feldman <feldman.sean@gmail.com>

---
+++ src/NServiceBus.Persistence.CosmosDB/Config/IProvideCosmosClient.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Cosmos;
+
+namespace NServiceBus
+{
+    /// <summary>
+    /// Public interface for providing a CosmosClient via dependency injection
+    /// </summary>
+    public interface IProvideCosmosClient
+    {
+        /// <summary>
+        /// The provided CosmosClient
+        /// </summary>
+        CosmosClient Client { get; }
+    }
+}
---
+++ src/NServiceBus.Persistence.CosmosDB/Config/ThrowIfNoCosmosClientIsProvided.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.Azure.Cosmos;
+
+namespace NServiceBus.Persistence.CosmosDB
+{
+    class ThrowIfNoCosmosClientIsProvided : IProvideCosmosClient
+    {
+        public CosmosClient Client => throw new Exception("No CosmosClient has been configured.");
+    }
+}
