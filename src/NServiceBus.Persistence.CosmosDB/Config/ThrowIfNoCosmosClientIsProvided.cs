292748781927baeab74453b3b03dcabfb671ffdf
danielmarbach <daniel.marbach@openplace.net>
2020-09-16 08:58:24 +0200

    Cleanup ThrowIfNoCosmosClientIsProvided

--- src/NServiceBus.Persistence.CosmosDB/Config/ThrowIfNoCosmosClientIsProvided.cs
+++ src/NServiceBus.Persistence.CosmosDB/Config/ThrowIfNoCosmosClientIsProvided.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using Microsoft.Azure.Cosmos;
-
-namespace NServiceBus.Persistence.CosmosDB
+﻿namespace NServiceBus.Persistence.CosmosDB
 {
+    using System;
+    using Microsoft.Azure.Cosmos;
+
     class ThrowIfNoCosmosClientIsProvided : IProvideCosmosClient
     {
         public CosmosClient Client => throw new Exception($"No CosmosClient has been configured. Either use `persistence.CosmosClient(client)` or register an implementation of `{nameof(IProvideCosmosClient)}` in the container.");
