d7e4c2d0284ccfed8a3f57fc0bb74aae3cf43540
danielmarbach <daniel.marbach@openplace.net>
2020-09-16 08:58:10 +0200

    Slightly more verbose exception message in ThrowIfNoCosmosClientIsProvided

--- src/NServiceBus.Persistence.CosmosDB/Config/ThrowIfNoCosmosClientIsProvided.cs
+++ src/NServiceBus.Persistence.CosmosDB/Config/ThrowIfNoCosmosClientIsProvided.cs
@@ -5,6 +5,6 @@ namespace NServiceBus.Persistence.CosmosDB
 {
     class ThrowIfNoCosmosClientIsProvided : IProvideCosmosClient
     {
-        public CosmosClient Client => throw new Exception("No CosmosClient has been configured.");
+        public CosmosClient Client => throw new Exception($"No CosmosClient has been configured. Either use `persistence.CosmosClient(client)` or register an implementation of `{nameof(IProvideCosmosClient)}` in the container.");
     }
 }
