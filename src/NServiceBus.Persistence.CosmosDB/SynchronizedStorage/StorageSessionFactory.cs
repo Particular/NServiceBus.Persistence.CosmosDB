e5b0adc900f3c27f09bd59242d168f2f8da74c74
Bob Langley <bob.langley@particular.net>
2020-09-17 10:42:59 -0700

    Add comment to null-conditional for future developers
    Co-authored-by: Sean Feldman <feldman.sean@gmail.com>

--- src/NServiceBus.Persistence.CosmosDB/SynchronizedStorage/StorageSessionFactory.cs
+++ src/NServiceBus.Persistence.CosmosDB/SynchronizedStorage/StorageSessionFactory.cs
@@ -14,6 +14,10 @@
         public Task<CompletableSynchronizedStorageSession> OpenSession(ContextBag contextBag)
         {
             var storageSession = new StorageSession(containerHolderResolver, contextBag, true);
+
+            //The null-conditional is to allow the PersistenceTests to run.
+            //CurrentSharedTransactionalBatchBehavior.Invoke calls CreateScope which is needed for SetCurrent to work.
+            //This is a workaround since PersistenceTests do not execute behaviors.
             currentSharedTransactionalBatchHolder?.SetCurrent(storageSession);
             return Task.FromResult<CompletableSynchronizedStorageSession>(storageSession);
         }
