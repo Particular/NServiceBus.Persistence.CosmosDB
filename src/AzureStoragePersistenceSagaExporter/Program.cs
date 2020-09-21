bdf0cb7e14321e00942ee18379e83e6c4818701f
danielmarbach <daniel.marbach@openplace.net>
2020-09-21 11:49:33 +0200

    Very crude version with tons of assumptions running

--- src/AzureStoragePersistenceSagaExporter/Program.cs
+++ src/AzureStoragePersistenceSagaExporter/Program.cs
@@ -1,10 +1,15 @@
 ﻿namespace Particular.AzureStoragePersistenceSagaExporter
 {
+    using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
+    using System.Text.RegularExpressions;
     using System.Threading.Tasks;
     using McMaster.Extensions.CommandLineUtils;
     using Microsoft.Azure.Cosmos.Table;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
 
     class Program
     {
@@ -31,9 +36,18 @@
 
                 var tableName = sagaTypeFullName.Split('.').Last();
 
+                if (!Directory.Exists(tableName))
+                {
+                    Directory.CreateDirectory(tableName);
+                }
+
                 var table = client.GetTableReference(tableName);
 
                 var query = new TableQuery<DictionaryTableEntity>();
+
+                var regex = new Regex("Index_(?:.*)_(?<PropertyName>.*)_\"(?<PropertyValue>.*)\"#");
+                var probablyJArrayRegex = new Regex("\\[.*\\]", RegexOptions.Multiline);
+                var probablyJObjectRegex = new Regex("\\{.*\\}", RegexOptions.Multiline);
 
                 await foreach (var entity in table.ExecuteQueryAsync(query, ct: cancellationToken))
                 {
@@ -41,17 +55,104 @@
                     {
                         continue;
                     }
+
+                    // NServiceBus_2ndIndexKey
+                    // Index_OrderSagaData_OrderId_"a3413eda-fb98-46c1-a44e-89da9efada16"#
+
+                    var match = regex.Match(entity["NServiceBus_2ndIndexKey"].StringValue);
+                    var propertyName = match.Groups["PropertyName"].Value;
+                    var propertyValue = match.Groups["PropertyValue"].Value;
+
+                    entity.Remove("NServiceBus_2ndIndexKey");
+                    entity.Remove("PartitionKey");
+                    entity.Remove("RowKey");
+
+                    var newSagaId = SagaIdGenerator.Generate(sagaTypeFullName, propertyName, propertyValue);
+                    var oldSagaId = entity["Id"].GuidValue;
+
+                    var root = new JObject();
+
+                    // maybe somehow use the constants from the persister, currently hardcoded to move forward
+                    var metadata = new JObject
+                    {
+                        { "SagaDataContainer-SchemaVersion", "1.0.0" },
+                        { "SagaDataContainer-FullTypeName", sagaTypeFullName }, // just some random proposals to move forward
+                        { "SagaDataContainer-OldSagaId", oldSagaId.ToString() } // just some random proposals to move forward
+                    };
+                    root.Add("_NServiceBus-Persistence-Metadata", metadata);
+
+                    root.Add("_id", newSagaId);
+
+                    foreach (var property in entity)
+                    {
+                        switch (property.Value.PropertyType)
+                        {
+                            case EdmType.String when probablyJArrayRegex.IsMatch(property.Value.StringValue):
+                                try
+                                {
+                                    var propertyAsJArray = JArray.Parse(property.Value.StringValue);
+                                    root.Add(property.Key, propertyAsJArray);
+                                }
+                                catch (JsonReaderException)
+                                {
+                                    // TODO: What to do here?
+                                }
+
+                                break;
+                            case EdmType.String when probablyJObjectRegex.IsMatch(property.Value.StringValue):
+                                try
+                                {
+                                    var propertyAsJObject = JObject.Parse(property.Value.StringValue);
+                                    root.Add(property.Key, propertyAsJObject);
+                                }
+                                catch (JsonReaderException)
+                                {
+                                    // TODO: What to do here?
+                                }
+                                break;
+                            case EdmType.Binary:
+                                root.Add(property.Key, property.Value.BinaryValue);
+                                break;
+                            case EdmType.Boolean:
+                                root.Add(property.Key, property.Value.BooleanValue);
+                                break;
+                            case EdmType.DateTime:
+                                if (property.Value.DateTimeOffsetValue.HasValue)
+                                {
+                                    root.Add(property.Key, property.Value.DateTimeOffsetValue.Value);
+                                }
+                                else
+                                {
+                                    root.Add(property.Key, property.Value.DateTime);
+                                }
+                                break;
+                            case EdmType.Double:
+                                root.Add(property.Key, property.Value.DoubleValue);
+                                break;
+                            case EdmType.Guid:
+                                root.Add(property.Key, property.Value.GuidValue);
+                                break;
+                            case EdmType.Int32:
+                                root.Add(property.Key, property.Value.Int32Value);
+                                break;
+                            case EdmType.Int64:
+                                root.Add(property.Key, property.Value.Int64Value);
+                                break;
+                            default:
+                                root.Add(property.Key, property.Value.StringValue);
+                                break;
+                        }
+                    }
+
+                    await using var fileWriter = File.CreateText(Path.Combine(tableName, $"{newSagaId}.json"));
+                    using var jsonTextWriter = new JsonTextWriter(fileWriter)
+                    {
+                        Formatting = Formatting.Indented
+                    };
+                    await root.WriteToAsync(jsonTextWriter, cancellationToken).ConfigureAwait(false);
+                    await jsonTextWriter.FlushAsync(cancellationToken).ConfigureAwait(false);
+                    await fileWriter.FlushAsync().ConfigureAwait(false);
                 }
-
-                //Each tableEntity
-                //  - Record primary key as old saga id
-                //  - Calc new saga id - parse secondary index
-                //  - To JObject
-                //  - Detect and convert string values that are serialized objects/arrays
-                //  - Remove PartitionKey/RowKey/secondary index properties
-                //  - Add metadata (old saga id / saga full type name)
-                //  - Dump file to current folder
-
                 return 0;
             });
 
---
+++ src/AzureStoragePersistenceSagaExporter/SagaIdGenerator.cs
@@ -0,0 +1,24 @@
+﻿namespace Particular.AzureStoragePersistenceSagaExporter
+{
+    using System;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    static class SagaIdGenerator
+    {
+        public static Guid Generate(string sagaEntityType, string correlationPropertyName, string correlationPropertyValue)
+        {
+            return DeterministicGuid($"{sagaEntityType}_{correlationPropertyName}_{correlationPropertyValue}");
+        }
+
+        static Guid DeterministicGuid(string src)
+        {
+            var stringBytes = Encoding.UTF8.GetBytes(src);
+
+            using var sha1CryptoServiceProvider = new SHA1CryptoServiceProvider();
+            var hashedBytes = sha1CryptoServiceProvider.ComputeHash(stringBytes);
+            Array.Resize(ref hashedBytes, 16);
+            return new Guid(hashedBytes);
+        }
+    }
+}
