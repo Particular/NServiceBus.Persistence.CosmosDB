3d11823e6102444c97afce3442a3dba74bb041b8
Bob Langley <bob.langley@particular.net>
2020-09-22 15:38:03 -0700

    Add a --version option
    Co-authored-by: Sean Feldman <feldman.sean@gmail.com>

--- src/AzureStoragePersistenceSagaExporter/Program.cs
+++ src/AzureStoragePersistenceSagaExporter/Program.cs
@@ -3,6 +3,7 @@
     using System.IO;
     using System.Threading.Tasks;
     using McMaster.Extensions.CommandLineUtils;
+    using Microsoft.Extensions.Logging;
 
     class Program
     {
@@ -14,6 +15,7 @@
             };
 
             var verboseOption = app.Option("-v|--verbose", "Show verbose output", CommandOptionType.NoValue, true);
+            var versionOption = app.Option("--version", "Show the current version of the tool", CommandOptionType.NoValue, true);
             var sagaDataNameOption = app.Option<string>($"-s|--{ApplicationOptions.SagaDataName}", "The saga data class name (w/o namespace) of the saga data to export. This will be the table name.", CommandOptionType.SingleValue);
             var connectionStringOption = app.Option<string>($"-c|--{ApplicationOptions.ConnectionString}", "The connection string to the Azure Storage account with the saga data.", CommandOptionType.SingleValue);
 
@@ -22,6 +24,13 @@
             app.OnExecuteAsync(cancellationToken =>
             {
                 var logger = new ConsoleLogger(verboseOption.HasValue());
+
+                logger.LogInformation($"export-aspsagas {GitVersionInformation.NuGetVersionV2} (Sha:{GitVersionInformation.ShortSha})");
+
+                if (versionOption.HasValue())
+                {
+                    return Task.CompletedTask;
+                }
 
                 return Exporter.Run(logger, connectionStringOption.Value(), sagaDataNameOption.Value(), Directory.GetCurrentDirectory(), cancellationToken);
             });
