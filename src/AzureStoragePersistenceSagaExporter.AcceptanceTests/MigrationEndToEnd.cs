bb1804d8345de506652beb54d537b86f61c5273e
danielmarbach <daniel.marbach@openplace.net>
2020-09-22 10:00:47 +0200

    Slightly more complex saga data

--- src/AzureStoragePersistenceSagaExporter.AcceptanceTests/MigrationEndToEnd.cs
+++ src/AzureStoragePersistenceSagaExporter.AcceptanceTests/MigrationEndToEnd.cs
@@ -1,7 +1,9 @@
 ﻿namespace AzureStoragePersistenceSagaExporter.AcceptanceTests
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
+    using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.Azure.Cosmos;
@@ -153,6 +155,35 @@
             public class MigratingSagaData : ContainSagaData
             {
                 public Guid MyId { get; set; }
+                public List<string> ListOfStrings { get; set; } = new List<string>
+                {
+                    "Hello World"
+                };
+
+                public List<int> ListOfINts { get; set; } = new List<int>
+                {
+                    43, 42
+                };
+
+                public Nested Nested { get; set; } = new Nested();
+
+                public int IntValue { get; set; } = 1;
+                public long LongValue { get; set; } = 1;
+                public double DoubleValue { get; set; } = 1.24;
+                public byte[] BinaryValue { get; set; } = Encoding.UTF8.GetBytes("Hello World");
+                public DateTime DateTimeValue { get; set; } = new DateTime(2020, 09, 21, 5, 5, 5, 5);
+                public bool BooleanValue { get; set; } = true;
+                public decimal DecimalValue { get; set; } = 1.2m;
+                public float FloatValue { get; set; } = 1.2f;
+
+                public string PretendsToBeAnArray { get; set; } = "[ Garbage ]";
+                public string PretendsToBeAnObject { get; set; } = "{ \"Garbage\" }";
+            }
+
+            public class Nested
+            {
+                public string Foo { get; set; } = "Foo";
+                public string Bar { get; set; } = "Bar";
             }
         }
 
