643379743285859ecf14fee81679208b7c9604f4
David Boike <david.boike@gmail.com>
2021-07-29 13:55:48 -0500

    Add pragmas for referencing obsolete MessageDrivenSubscriptions

--- src/NServiceBus.Persistence.CosmosDB/Utility/NonNativePubSubCheck.cs
+++ src/NServiceBus.Persistence.CosmosDB/Utility/NonNativePubSubCheck.cs
@@ -7,10 +7,12 @@
     {
         public static void ThrowIfMessageDrivenPubSubInUse(FeatureConfigurationContext context)
         {
+#pragma warning disable CS0618 // Type or member is obsolete - Remove this method along with MessageDrivenSubscriptions
             if (context.Settings.IsFeatureEnabled(typeof(MessageDrivenSubscriptions)) || context.Settings.IsFeatureActive(typeof(MessageDrivenSubscriptions)))
             {
                 throw new Exception("NServiceBus.Persistence.CosmosDB must be used with a transport that provides native publish/subscribe capabilities, and cannot be used with message-driven publish/subscribe.");
             }
+#pragma warning restore CS0618 // Type or member is obsolete
         }
     }
 }
