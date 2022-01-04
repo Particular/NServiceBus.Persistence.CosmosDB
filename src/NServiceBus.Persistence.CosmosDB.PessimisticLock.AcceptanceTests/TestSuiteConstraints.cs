21185ec7e6d542aef805ccd97f2c35f6783f9ba5
danielmarbach <daniel.marbach@openplace.net>
2022-01-04 17:47:30 +0100

    Add pessimistic lock acceptance tests

---
+++ src/NServiceBus.Persistence.CosmosDB.PessimisticLock.AcceptanceTests/TestSuiteConstraints.cs
@@ -0,0 +1,25 @@
+﻿namespace NServiceBus.AcceptanceTests
+{
+    using AcceptanceTesting.Support;
+
+    public partial class TestSuiteConstraints
+    {
+        public bool SupportsDtc { get; } = false;
+        public bool SupportsCrossQueueTransactions { get; } = true;
+        public bool SupportsNativePubSub { get; } = true;
+        public bool SupportsNativeDeferral { get; } = true;
+        public bool SupportsOutbox { get; } = false;
+        public bool SupportsDelayedDelivery { get; } = true;
+        public bool SupportsPurgeOnStartup { get; } = true;
+
+        public IConfigureEndpointTestExecution CreateTransportConfiguration()
+        {
+            return new ConfigureEndpointAcceptanceTestingTransport(true, true);
+        }
+
+        public IConfigureEndpointTestExecution CreatePersistenceConfiguration()
+        {
+            return new ConfigureEndpointCosmosDBPersistence();
+        }
+    }
+}
