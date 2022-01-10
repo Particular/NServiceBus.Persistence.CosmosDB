be42b512acd8fb03f96410510a9d898547c7f794
danielmarbach <daniel.marbach@openplace.net>
2022-01-10 08:45:58 +0100

    Add acceptance test for migrated saga with high contention

---
+++ src/NServiceBus.Persistence.CosmosDB.PessimisticLock.AcceptanceTests/When_storing_migrated_saga_with_high_contention.cs
@@ -0,0 +1,186 @@
+﻿namespace NServiceBus.AcceptanceTests
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using EndpointTemplates;
+    using Microsoft.Azure.Cosmos;
+    using NUnit.Framework;
+    using Persistence.CosmosDB;
+    using Headers = NServiceBus.Headers;
+
+    [TestFixture]
+    public class When_storing_migrated_saga_with_high_contention : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_succeed_without_retries()
+        {
+            var context = await Scenario.Define<HighContentionScenario>()
+                .WithEndpoint<HighContentionEndpointWithMigrationMode>(behavior =>
+                {
+                    behavior.When(async (session, scenarioContext) =>
+                    {
+                        await ImportIntoCosmosDB(scenarioContext);
+                        await Task.WhenAll(Enumerable.Range(0, scenarioContext.ConcurrentMessageCount).Select(_ =>
+                        {
+                            var options = new SendOptions();
+                            options.RouteToThisEndpoint();
+                            options.SetHeader(Headers.SagaId, scenarioContext.MigratedSagaId.ToString());
+                            return session.Send(new ConcurrentMessage { SomeId = scenarioContext.SomeId }, options);
+                        }));
+                        scenarioContext.ConcurrentMessagesSent = true;
+                    });
+                })
+                .Done(s => s.SagaCompleted)
+                .Run();
+
+            Assert.IsTrue(context.ConcurrentMessagesSent);
+            Assert.AreEqual(0, context.RetryCount);
+        }
+
+        static string MigrationDocument = @"{{
+    ""_NServiceBus-Persistence-Metadata"": {{
+        ""SagaDataContainer-SchemaVersion"": ""1.0.0"",
+        ""SagaDataContainer-FullTypeName"": ""NServiceBus.AcceptanceTests.When_storing_migrated_saga_with_high_contention+HighContentionEndpoint+HighContentionSaga"",
+        ""SagaDataContainer-MigratedSagaId"": ""{0}""
+    }},
+    ""id"": ""{1}"",
+    ""SomeId"": ""{2}"",
+    ""HitCount"": 0,
+    ""Originator"": ""NServiceBus.AcceptanceTests.HighContentionEndpoint"",
+    ""OriginalMessageId"": ""6492af3f-1d60-43f6-8e62-ae1600ab23a2""
+}}";
+
+        static async Task ImportIntoCosmosDB(HighContentionScenario scenarioContext)
+        {
+            var container = SetupFixture.Container;
+
+            var actualSagaId = CosmosSagaIdGenerator.Generate(typeof(HighContentionEndpointWithMigrationMode.HighContentionSaga),
+                nameof(HighContentionEndpointWithMigrationMode.HighContentionSaga.HighContentionSagaData.SomeId), scenarioContext.SomeId);
+
+            string document = string.Format(MigrationDocument, scenarioContext.MigratedSagaId, actualSagaId, scenarioContext.SomeId);
+            using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(document)))
+            {
+                var response = await container.CreateItemStreamAsync(stream, new PartitionKey(actualSagaId.ToString()));
+
+                Assert.IsTrue(response.IsSuccessStatusCode, "Successfully imported");
+            }
+        }
+
+        public class HighContentionScenario : ScenarioContext
+        {
+            long retryCount;
+
+            public int ConcurrentMessageCount => 20;
+
+            public bool ConcurrentMessagesSent { get; set; }
+
+            public bool SagaCompleted { get; set; }
+
+            public Guid SomeId { get; } = Guid.NewGuid();
+
+            public Guid MigratedSagaId { get; } = Guid.NewGuid();
+
+            public long RetryCount => Interlocked.Read(ref retryCount);
+
+            public void IncrementRetryCount() => Interlocked.Increment(ref retryCount);
+        }
+
+        class HighContentionEndpointWithMigrationMode : EndpointConfigurationBuilder
+        {
+            public HighContentionEndpointWithMigrationMode()
+            {
+                EndpointSetup<DefaultServer, HighContentionScenario>((endpoint, scenario) =>
+                {
+                    var persistence = endpoint.UsePersistence<CosmosPersistence>();
+                    persistence.Sagas().EnableMigrationMode();
+
+                    endpoint.LimitMessageProcessingConcurrencyTo(scenario.ConcurrentMessageCount);
+
+                    var recoverability = endpoint.Recoverability();
+
+                    recoverability.Immediate(immediateRetries =>
+                    {
+                        immediateRetries.OnMessageBeingRetried((m, _) =>
+                        {
+                            scenario.IncrementRetryCount();
+                            return Task.CompletedTask;
+                        });
+
+                        immediateRetries.NumberOfRetries(scenario.ConcurrentMessageCount);
+                    });
+
+                    recoverability.Delayed(s => s.NumberOfRetries(0));
+                });
+            }
+
+            internal class HighContentionSaga : Saga<HighContentionSaga.HighContentionSagaData>, IAmStartedByMessages<StartSaga>, IHandleMessages<ConcurrentMessage>
+            {
+                readonly HighContentionScenario scenario;
+
+                public HighContentionSaga(HighContentionScenario scenario) => this.scenario = scenario;
+
+                protected override void ConfigureHowToFindSaga(SagaPropertyMapper<HighContentionSagaData> mapper)
+                {
+                    mapper.ConfigureMapping<StartSaga>(message => message.SomeId).ToSaga(data => data.SomeId);
+                    mapper.ConfigureMapping<ConcurrentMessage>(message => message.SomeId).ToSaga(data => data.SomeId);
+                }
+
+                // will never be called
+                public Task Handle(StartSaga message, IMessageHandlerContext context) => Task.CompletedTask;
+
+                public class HighContentionSagaData : ContainSagaData
+                {
+                    public Guid SomeId { get; set; }
+
+                    public int HitCount { get; set; }
+                }
+
+                public async Task Handle(ConcurrentMessage message, IMessageHandlerContext context)
+                {
+                    Data.HitCount++;
+
+                    if (Data.HitCount >= scenario.ConcurrentMessageCount)
+                    {
+                        MarkAsComplete();
+                        await context.SendLocal(new SagaCompleted { SomeId = message.SomeId, HitCount = Data.HitCount });
+                    }
+                }
+            }
+
+            class DoneHandler : IHandleMessages<SagaCompleted>
+            {
+                readonly HighContentionScenario scenario;
+
+                public DoneHandler(HighContentionScenario scenario) => this.scenario = scenario;
+
+                public Task Handle(SagaCompleted message, IMessageHandlerContext context)
+                {
+                    scenario.SagaCompleted = true;
+                    return Task.CompletedTask;
+                }
+            }
+        }
+
+        public class StartSaga : IMessage
+        {
+            public Guid SomeId { get; set; }
+        }
+
+        public class ConcurrentMessage : IMessage
+        {
+            public Guid SomeId { get; set; }
+        }
+
+        public class SagaCompleted : IMessage
+        {
+            public Guid SomeId { get; set; }
+
+            public int HitCount { get; set; }
+        }
+    }
+}
