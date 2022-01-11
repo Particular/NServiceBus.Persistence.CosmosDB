24dacd960af5a6a8f2473798add11e11ffe65482
danielmarbach <daniel.marbach@openplace.net>
2022-01-11 15:27:09 +0100

    Documentation

--- src/NServiceBus.Persistence.CosmosDB/Transaction/TransactionInformationConfiguration.cs
+++ src/NServiceBus.Persistence.CosmosDB/Transaction/TransactionInformationConfiguration.cs
@@ -9,64 +9,70 @@
     public class TransactionInformationConfiguration
     {
         /// <summary>
-        ///
+        /// Adds an instance of <see cref="ITransactionInformationFromHeadersExtractor"/> to the list of header extractors.
         /// </summary>
-        /// <param name="extractor"></param>
+        /// <remarks>Explicitly added extractors and extraction rules are executed before extractors registered on the container.</remarks>
         public void ExtractFromHeaders(ITransactionInformationFromHeadersExtractor extractor) => Extractor.ExtractFromHeaders(extractor);
 
         /// <summary>
-        ///
+        /// Adds an instance of <see cref="ITransactionInformationFromMessagesExtractor"/> to the list of header extractors.
         /// </summary>
-        /// <param name="extractor"></param>
+        /// <remarks>Explicitly added extractors and extraction rules are executed before extractors registered on the container.</remarks>
         public void ExtractFromMessages(ITransactionInformationFromMessagesExtractor extractor) => Extractor.ExtractFromMessages(extractor);
 
         /// <summary>
-        ///
+        /// Adds an extraction rule that extracts the partition key from a given message type <typeparamref name="TMessage"/>
         /// </summary>
-        /// <param name="extractor"></param>
-        /// <param name="containerInformation"></param>
-        /// <typeparam name="TMessage"></typeparam>
+        /// <param name="extractor">The extraction function.</param>
+        /// <param name="containerInformation">The optional container information to be used when this rule matches.</param>
+        /// <typeparam name="TMessage">The message type to match against.</typeparam>
+        /// <remarks>Explicitly added extractors and extraction rules are executed before extractors registered on the container.</remarks>
         public void ExtractFromMessage<TMessage>(Func<TMessage, PartitionKey> extractor, ContainerInformation? containerInformation = default) =>
             Extractor.ExtractFromMessage(extractor, containerInformation);
 
         /// <summary>
-        ///
+        /// Adds an extraction rule that extracts the partition key from a given message type <typeparamref name="TMessage"/>
         /// </summary>
-        /// <param name="extractor"></param>
-        /// <param name="containerInformation"></param>
-        /// <param name="extractorArgument"></param>
-        /// <typeparam name="TMessage"></typeparam>
-        /// <typeparam name="TArg"></typeparam>
+        /// <param name="extractor">The extraction function.</param>
+        /// <param name="containerInformation">The optional container information to be used when this rule matches.</param>
+        /// <param name="extractorArgument">The argument passed as state to the <paramref name="extractor"/></param>
+        /// <typeparam name="TMessage">The message type to match against.</typeparam>
+        /// <typeparam name="TArg">The argument type.</typeparam>
+        /// <remarks>Explicitly added extractors and extraction rules are executed before extractors registered on the container.</remarks>
         public void ExtractFromMessage<TMessage, TArg>(Func<TMessage, TArg, PartitionKey> extractor, TArg extractorArgument, ContainerInformation? containerInformation = default) =>
             Extractor.ExtractFromMessage(extractor, extractorArgument, containerInformation);
 
 
         /// <summary>
-        ///
+        /// Adds an extraction rule that extracts the partition key from a given header represented by <paramref name="headerKey"/>.
         /// </summary>
-        /// <param name="headerKey"></param>
-        /// <param name="converter"></param>
-        /// <param name="containerInformation"></param>
+        /// <param name="headerKey">The header key.</param>
+        /// <param name="converter">The converter function to convert the header value.</param>
+        /// <param name="containerInformation">The optional container information to be used when this rule matches.</param>
+        /// <remarks>Explicitly added extractors and extraction rules are executed before extractors registered on the container.</remarks>
         public void ExtractFromHeader(string headerKey, Func<string, string> converter,
             ContainerInformation? containerInformation = default) =>
             Extractor.ExtractFromHeader(headerKey, converter, containerInformation);
 
+
         /// <summary>
-        ///
+        /// Adds an extraction rule that extracts the partition key from a given header represented by <paramref name="headerKey"/>.
         /// </summary>
-        /// <param name="headerKey"></param>
-        /// <param name="converter"></param>
-        /// <param name="containerInformation"></param>
-        /// <param name="converterArgument"></param>
-        /// <typeparam name="TArg"></typeparam>
+        /// <param name="headerKey">The header key.</param>
+        /// <param name="converter">The converter function to convert the header value.</param>
+        /// <param name="containerInformation">The optional container information to be used when this rule matches.</param>
+        /// <param name="converterArgument">The argument passed as state to the <paramref name="converter"/></param>
+        /// <typeparam name="TArg">The argument type.</typeparam>
+        /// <remarks>Explicitly added extractors and extraction rules are executed before extractors registered on the container.</remarks>
         public void ExtractFromHeader<TArg>(string headerKey, Func<string, TArg, string> converter, TArg converterArgument, ContainerInformation? containerInformation = default) =>
             Extractor.ExtractFromHeader(headerKey, converter, converterArgument, containerInformation);
 
         /// <summary>
-        ///
+        /// Adds an extraction rule that extracts the partition key from a given header represented by <paramref name="headerKey"/>.
         /// </summary>
-        /// <param name="headerKey"></param>
-        /// <param name="containerInformation"></param>
+        /// <param name="headerKey">The header key.</param>
+        /// <param name="containerInformation">The optional container information to be used when this rule matches.</param>
+        /// <remarks>Explicitly added extractors and extraction rules are executed before extractors registered on the container.</remarks>
         public void ExtractFromHeader(string headerKey, ContainerInformation? containerInformation = default) =>
             Extractor.ExtractFromHeader(headerKey, containerInformation);
 
