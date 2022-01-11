a60e75774b026700f3970e5728ee9f6d08e23f95
danielmarbach <daniel.marbach@openplace.net>
2022-01-11 14:18:47 +0100

    Fix acceptance test

--- src/NServiceBus.Persistence.CosmosDB.LogicalOutbox.AcceptanceTests/When_custom_extractor_from_message_registered_via_container.cs
+++ src/NServiceBus.Persistence.CosmosDB.LogicalOutbox.AcceptanceTests/When_custom_extractor_from_message_registered_via_container.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Threading.Tasks;
     using AcceptanceTesting;
+    using AcceptanceTesting.Support;
     using EndpointTemplates;
     using Microsoft.Azure.Cosmos;
     using Microsoft.Extensions.DependencyInjection;
@@ -14,13 +15,16 @@
         [Test]
         public async Task Should_be_used()
         {
+            var runSettings = new RunSettings();
+            runSettings.DoNotRegisterDefaultPartitionKeyProvider();
+
             var context = await Scenario.Define<Context>()
                 .WithEndpoint<EndpointWithCustomExtractor>(b => b.When(session => session.SendLocal(new StartSaga1
                 {
                     DataId = Guid.NewGuid()
                 })))
                 .Done(c => c.SagaReceivedMessage)
-                .Run();
+                .Run(runSettings);
 
             Assert.True(context.ExtractorWasCalled);
         }
