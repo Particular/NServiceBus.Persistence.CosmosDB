6a229a3b7778452985d1df4ab973d3b48c8f9fe0
danielmarbach <daniel.marbach@openplace.net>
2022-01-11 11:46:15 +0100

    Extend fluent API test with message mapping

--- src/NServiceBus.Persistence.CosmosDB.PhysicalOutbox.AcceptanceTests/When_fluent_extractor_registered_via_api.cs
+++ src/NServiceBus.Persistence.CosmosDB.PhysicalOutbox.AcceptanceTests/When_fluent_extractor_registered_via_api.cs
@@ -5,6 +5,7 @@
     using AcceptanceTesting;
     using AcceptanceTesting.Support;
     using EndpointTemplates;
+    using Microsoft.Azure.Cosmos;
     using NUnit.Framework;
     using Persistence.CosmosDB;
 
@@ -23,18 +24,20 @@
                     sendOptions.RouteToThisEndpoint();
                     sendOptions.SetHeader("PartitionKeyHeader", ctx.TestRunId.ToString());
 
-                    return session.Send(new StartSaga1 { DataId = Guid.NewGuid() }, sendOptions);
+                    return session.Send(new StartSaga1 { DataId = ctx.TestRunId }, sendOptions);
                 }))
                 .Done(c => c.SagaReceivedMessage)
                 .Run(runSettings);
 
-            Assert.True(context.StateMatched);
+            Assert.True(context.HeaderStateMatched);
+            Assert.True(context.MessageStateMatched);
         }
 
         public class Context : ScenarioContext
         {
             public bool SagaReceivedMessage { get; set; }
-            public bool StateMatched { get; set; }
+            public bool HeaderStateMatched { get; set; }
+            public bool MessageStateMatched { get; set; }
         }
 
         public class EndpointWithFluentExtractor : EndpointConfigurationBuilder
@@ -46,8 +49,13 @@
                     var extractor = new TransactionInformationExtractor();
                     extractor.ExtractFromHeader("PartitionKeyHeader", (value, state) =>
                     {
-                        state.StateMatched = Guid.Parse(value).Equals(state.TestRunId);
+                        state.HeaderStateMatched = Guid.Parse(value).Equals(state.TestRunId);
                         return value;
+                    }, (Context)r.ScenarioContext, new ContainerInformation(SetupFixture.ContainerName, new PartitionKeyPath(SetupFixture.PartitionPathKey)));
+                    extractor.ExtractFromMessage<CompleteSagaMessage, Context>((message, state) =>
+                    {
+                        state.MessageStateMatched = message.DataId.Equals(state.TestRunId);
+                        return new PartitionKey(message.DataId.ToString());
                     }, (Context)r.ScenarioContext, new ContainerInformation(SetupFixture.ContainerName, new PartitionKeyPath(SetupFixture.PartitionPathKey)));
 
                     var persistence = config.UsePersistence<CosmosPersistence>();
@@ -55,7 +63,7 @@
                 });
             }
 
-            public class JustASaga : Saga<JustASagaData>, IAmStartedByMessages<StartSaga1>
+            public class JustASaga : Saga<JustASagaData>, IAmStartedByMessages<StartSaga1>, IHandleMessages<CompleteSagaMessage>
             {
                 public JustASaga(Context testContext)
                 {
@@ -65,6 +73,12 @@
                 public Task Handle(StartSaga1 message, IMessageHandlerContext context)
                 {
                     Data.DataId = message.DataId;
+
+                    return context.SendLocal(new CompleteSagaMessage { DataId = message.DataId });
+                }
+
+                public Task Handle(CompleteSagaMessage message, IMessageHandlerContext context)
+                {
                     testContext.SagaReceivedMessage = true;
                     MarkAsComplete();
                     return Task.CompletedTask;
@@ -73,6 +87,7 @@
                 protected override void ConfigureHowToFindSaga(SagaPropertyMapper<JustASagaData> mapper)
                 {
                     mapper.ConfigureMapping<StartSaga1>(m => m.DataId).ToSaga(s => s.DataId);
+                    mapper.ConfigureMapping<CompleteSagaMessage>(m => m.DataId).ToSaga(s => s.DataId);
                 }
 
                 readonly Context testContext;
@@ -85,6 +100,11 @@
         }
 
         public class StartSaga1 : ICommand
+        {
+            public Guid DataId { get; set; }
+        }
+
+        public class CompleteSagaMessage : ICommand
         {
             public Guid DataId { get; set; }
         }
