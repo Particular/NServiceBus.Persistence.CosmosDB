40047b0ce69794239feefdb1a6411fae1f3653fb
danielmarbach <daniel.marbach@openplace.net>
2022-01-11 11:28:55 +0100

    Revert "Very crude scanning logic"
    This reverts commit 9142e6f86749425745fddb50ca7e290ab7574d88.

--- src/NServiceBus.Persistence.CosmosDB/Transaction/Transaction.cs
+++ src/NServiceBus.Persistence.CosmosDB/Transaction/Transaction.cs
@@ -1,8 +1,6 @@
 namespace NServiceBus.Persistence.CosmosDB
 {
-    using System;
     using System.Collections.Generic;
-    using System.Linq;
     using Features;
     using Microsoft.Extensions.DependencyInjection;
 
@@ -20,46 +18,15 @@ namespace NServiceBus.Persistence.CosmosDB
         protected override void Setup(FeatureConfigurationContext context)
         {
             var extractTransactionInformationFromHeaders = context.Settings.Get<List<IExtractTransactionInformationFromHeaders>>();
-            var fromHeaderTypesAdded = new HashSet<Type>();
             foreach (var extractTransactionInformationFromHeader in extractTransactionInformationFromHeaders)
             {
                 context.Services.AddSingleton(extractTransactionInformationFromHeader);
-                fromHeaderTypesAdded.Add(extractTransactionInformationFromHeader.GetType());
             }
 
             var extractTransactionInformationFromMessages = context.Settings.Get<List<IExtractTransactionInformationFromMessages>>();
-            var fromMessageTypesAdded = new HashSet<Type>();
             foreach (var extractTransactionInformationFromMessage in extractTransactionInformationFromMessages)
             {
                 context.Services.AddSingleton(extractTransactionInformationFromMessage);
-                fromMessageTypesAdded.Add(extractTransactionInformationFromMessage.GetType());
-            }
-
-            // TODO: Add behind flag
-            var fromHeadersExtractorTypes =
-                from type in context.Settings.GetAvailableTypes()
-                let interfaces = type.GetInterfaces()
-                where interfaces.Any(t => t == typeof(IExtractTransactionInformationFromHeaders)) &&
-                      type.Assembly != GetType().Assembly &&
-                      !fromHeaderTypesAdded.Contains(type)
-                select type;
-
-            foreach (var headersExtractorType in fromHeadersExtractorTypes)
-            {
-                context.Services.AddSingleton(typeof(IExtractTransactionInformationFromHeaders), headersExtractorType);
-            }
-
-            var fromMessageExtractorTypes =
-                from type in context.Settings.GetAvailableTypes()
-                let interfaces = type.GetInterfaces()
-                where interfaces.Any(t => t == typeof(IExtractTransactionInformationFromMessages)) &&
-                      type.Assembly != GetType().Assembly &&
-                      !fromMessageTypesAdded.Contains(type)
-                select type;
-
-            foreach (var messageExtractorType in fromMessageExtractorTypes)
-            {
-                context.Services.AddSingleton(typeof(IExtractTransactionInformationFromMessages), messageExtractorType);
             }
 
             context.Pipeline.Register<TransactionInformationBeforeTheLogicalOutboxBehavior.RegisterStep>();
