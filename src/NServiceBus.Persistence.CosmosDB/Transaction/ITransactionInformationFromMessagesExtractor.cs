2c18c9fc1f116277c048d5f4607256e487ae4a05
Bob Langley <bob.langley@particular.net>
2022-01-12 09:41:18 -0800

    Merge pull request #290 from Particular/missing-xml-documentation
    More missing documentation

--- src/NServiceBus.Persistence.CosmosDB/Transaction/ITransactionInformationFromMessagesExtractor.cs
+++ src/NServiceBus.Persistence.CosmosDB/Transaction/ITransactionInformationFromMessagesExtractor.cs
@@ -3,17 +3,17 @@
     using Microsoft.Azure.Cosmos;
 
     /// <summary>
-    /// 
+    /// Extracts transaction information such as the <see cref="PartitionKey"/> and optionally the <see cref="ContainerInformation"/> from message instances.
     /// </summary>
     public interface ITransactionInformationFromMessagesExtractor
     {
         /// <summary>
-        /// 
+        /// Tries to extract a <see cref="PartitionKey"/> and optionally the <see cref="ContainerInformation"/> from message instance.
         /// </summary>
-        /// <param name="message"></param>
-        /// <param name="partitionKey"></param>
-        /// <param name="containerInformation"></param>
-        /// <returns></returns>
+        /// <param name="message">The currently received message instance.</param>
+        /// <param name="partitionKey">The <see cref="PartitionKey"/>.</param>
+        /// <param name="containerInformation">The optional <see cref="ContainerInformation"/>.</param>
+        /// <returns><c>true</c> if the <paramref name="partitionKey"/> is set; otherwise <c>false</c></returns>
         bool TryExtract(object message, out PartitionKey? partitionKey, out ContainerInformation? containerInformation);
     }
 }
