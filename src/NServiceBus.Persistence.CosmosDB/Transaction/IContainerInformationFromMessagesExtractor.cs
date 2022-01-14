32e4a25309f992e72b9401b59073effa63a3b0d9
danielmarbach <daniel.marbach@openplace.net>
2022-01-14 09:43:52 +0100

    New interfaces for better segregation

---
+++ src/NServiceBus.Persistence.CosmosDB/Transaction/IContainerInformationFromMessagesExtractor.cs
@@ -0,0 +1,23 @@
+﻿namespace NServiceBus.Persistence.CosmosDB
+{
+    using System.Collections.Generic;
+    using Microsoft.Azure.Cosmos;
+
+    /// <summary>
+    /// Extracts the <see cref="ContainerInformation"/> from message instances at the logical stage right after the payload has been deserialized.
+    /// Implementors of this interface have access to the message instance as well as the headers of the message instance.
+    /// The outbox mechanism might already have started and thus providing the <see cref="ContainerInformation"/> at this stage corresponds to the logical outbox phase.
+    /// In cases when only headers are required use <see cref="IContainerInformationFromHeadersExtractor"/> instead.
+    /// </summary>
+    public interface IContainerInformationFromMessagesExtractor
+    {
+        /// <summary>
+        /// Tries to extract a <see cref="PartitionKey"/> from a message instance.
+        /// </summary>
+        /// <param name="message">The currently received message instance.</param>
+        /// <param name="headers">The headers of the currently received message.</param>
+        /// <param name="containerInformation">The <see cref="ContainerInformation"/>.</param>
+        /// <returns><c>true</c> if the <paramref name="containerInformation"/> is set; otherwise <c>false</c></returns>
+        bool TryExtract(object message, IReadOnlyDictionary<string, string> headers, out ContainerInformation? containerInformation);
+    }
+}
---
+++ src/NServiceBus.Persistence.CosmosDB/Transaction/IPartitionKeyFromHeadersExtractor.cs
@@ -0,0 +1,22 @@
+﻿namespace NServiceBus.Persistence.CosmosDB
+{
+    using System.Collections.Generic;
+    using Microsoft.Azure.Cosmos;
+
+    /// <summary>
+    /// Extracts the <see cref="PartitionKey"/> from the headers at the physical stage when the message has not been deserialized
+    /// and the outbox mechanism hasn't started yet. This is the earliest possible point to
+    /// extract the <see cref="PartitionKey"/> in cases the partition key doesn't need to be extracted from the message instance.
+    /// Otherwise consider using <see cref="IPartitionKeyFromMessagesExtractor"/> instead.
+    /// </summary>
+    public interface IPartitionKeyFromHeadersExtractor
+    {
+        /// <summary>
+        /// Tries to extract a <see cref="PartitionKey"/> from the headers.
+        /// </summary>
+        /// <param name="headers">The headers of the currently received message.</param>
+        /// <param name="partitionKey">The <see cref="PartitionKey"/>.</param>
+        /// <returns><c>true</c> if the <paramref name="partitionKey"/> is set; otherwise <c>false</c></returns>
+        bool TryExtract(IReadOnlyDictionary<string, string> headers, out PartitionKey? partitionKey);
+    }
+}
