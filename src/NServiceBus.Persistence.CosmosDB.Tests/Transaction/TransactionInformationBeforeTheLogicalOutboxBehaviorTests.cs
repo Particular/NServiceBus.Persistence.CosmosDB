3415493a409d459ef172121bb9139c6851a98e35
danielmarbach <daniel.marbach@openplace.net>
2022-01-14 16:24:46 +0100

    Fix the behavior tests

--- src/NServiceBus.Persistence.CosmosDB.Tests/Transaction/TransactionInformationBeforeTheLogicalOutboxBehaviorTests.cs
+++ src/NServiceBus.Persistence.CosmosDB.Tests/Transaction/TransactionInformationBeforeTheLogicalOutboxBehaviorTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace NServiceBus.Persistence.CosmosDB.Tests.Transaction
 {
+    using System.Collections.Generic;
     using System.Threading.Tasks;
     using Microsoft.Azure.Cosmos;
     using NUnit.Framework;
@@ -11,38 +12,35 @@
     public class TransactionInformationBeforeTheLogicalOutboxBehaviorTests
     {
         [Test]
-        public async Task Should_not_set_partition_key_or_container_when_extractor_returns_false()
+        public async Task Should_not_set_partition_key_when_partition_key_extractor_returns_false()
         {
-            var extractor = new Extractor(
-                delegate (object msg, out PartitionKey? key, out ContainerInformation? container)
+            var extractor = new PartitionKeyExtractor(
+                (object msg, IReadOnlyDictionary<string, string> headers, out PartitionKey? key) =>
                 {
                     key = null;
-                    container = null;
                     return false;
                 });
 
-            var behavior = new TransactionInformationBeforeTheLogicalOutboxBehavior(extractor);
+            var behavior = new TransactionInformationBeforeTheLogicalOutboxBehavior(extractor, new ContainerInformationExtractor());
 
             var context = new TestableIncomingLogicalMessageContext();
 
             await behavior.Invoke(context, _ => Task.CompletedTask);
 
             Assert.That(context.Extensions.TryGet<PartitionKey>(out _), Is.False);
-            Assert.That(context.Extensions.TryGet<ContainerInformation>(out _), Is.False);
         }
 
         [Test]
-        public async Task Should_set_partition_key_when_extractor_returns_true()
+        public async Task Should_set_partition_key_when_partition_key_extractor_returns_true()
         {
-            var extractor = new Extractor(
-                delegate (object msg, out PartitionKey? key, out ContainerInformation? container)
+            var partitionKeyExtractor = new PartitionKeyExtractor(
+                (object msg, IReadOnlyDictionary<string, string> headers, out PartitionKey? key) =>
                 {
                     key = new PartitionKey(true);
-                    container = null;
                     return true;
                 });
 
-            var behavior = new TransactionInformationBeforeTheLogicalOutboxBehavior(extractor);
+            var behavior = new TransactionInformationBeforeTheLogicalOutboxBehavior(partitionKeyExtractor, new ContainerInformationExtractor());
 
             var context = new TestableIncomingLogicalMessageContext();
 
@@ -53,17 +51,35 @@
         }
 
         [Test]
-        public async Task Should_set_container_when_extractor_returns_true()
+        public async Task Should_not_set_container_information_when_container_information_extractor_returns_false()
         {
-            var extractor = new Extractor(
-                delegate (object msg, out PartitionKey? key, out ContainerInformation? container)
+            var extractor = new ContainerInformationExtractor(
+                (object msg, IReadOnlyDictionary<string, string> headers, out ContainerInformation? container) =>
                 {
-                    key = null;
+                    container = null;
+                    return false;
+                });
+
+            var behavior = new TransactionInformationBeforeTheLogicalOutboxBehavior(new CosmosDB.PartitionKeyExtractor(), extractor);
+
+            var context = new TestableIncomingLogicalMessageContext();
+
+            await behavior.Invoke(context, _ => Task.CompletedTask);
+
+            Assert.That(context.Extensions.TryGet<ContainerInformation>(out _), Is.False);
+        }
+
+        [Test]
+        public async Task Should_set_container_when_container_information_extractor_returns_true()
+        {
+            var extractor = new ContainerInformationExtractor(
+                (object msg, IReadOnlyDictionary<string, string> headers, out ContainerInformation? container) =>
+                {
                     container = new ContainerInformation("containerName", new PartitionKeyPath("/deep/down"));
                     return true;
                 });
 
-            var behavior = new TransactionInformationBeforeTheLogicalOutboxBehavior(extractor);
+            var behavior = new TransactionInformationBeforeTheLogicalOutboxBehavior(new PartitionKeyExtractor(), extractor);
 
             var context = new TestableIncomingLogicalMessageContext();
 
@@ -74,41 +90,116 @@
         }
 
         [Test]
-        public async Task Should_pass_message()
+        public async Task Should_pass_message_and_headers_to_partition_key_extractor()
         {
-            object capturedMessageInstance = null;
-            var extractor = new Extractor(
-                delegate (object msg, out PartitionKey? key, out ContainerInformation? container)
+            object capturedMessageInstanceFromPartionKeyExtractor = null;
+            IReadOnlyDictionary<string, string> capturedHeadersFromPartionKeyExtractor = null;
+            var partitionKeyExtractor = new PartitionKeyExtractor(
+                (object msg, IReadOnlyDictionary<string, string> headers, out PartitionKey? partitionKey) =>
                 {
-                    key = null;
-                    container = null;
-                    capturedMessageInstance = msg;
+                    partitionKey = null;
+                    capturedMessageInstanceFromPartionKeyExtractor = msg;
+                    capturedHeadersFromPartionKeyExtractor = headers;
                     return true;
                 });
 
-            var behavior = new TransactionInformationBeforeTheLogicalOutboxBehavior(extractor);
+            var behavior = new TransactionInformationBeforeTheLogicalOutboxBehavior(partitionKeyExtractor, new ContainerInformationExtractor());
 
-            var context = new TestableIncomingLogicalMessageContext();
+            var messageHeaders = new Dictionary<string, string> { { "HeaderKey", "HeaderValue" } };
             var messageInstance = new object();
-            context.Message = new LogicalMessage(new MessageMetadata(typeof(object)), messageInstance);
+            var context = new TestableIncomingLogicalMessageContext
+            {
+                MessageHeaders = messageHeaders,
+                Message = new LogicalMessage(new MessageMetadata(typeof(object)), messageInstance)
+            };
 
             await behavior.Invoke(context, _ => Task.CompletedTask);
 
-            Assert.That(capturedMessageInstance, Is.Not.Null.And.EqualTo(messageInstance));
+            Assert.That(capturedMessageInstanceFromPartionKeyExtractor, Is.Not.Null.And.EqualTo(messageInstance));
+            Assert.That(capturedHeadersFromPartionKeyExtractor, Is.Not.Null.And.EqualTo(messageHeaders));
         }
 
-        delegate bool TryExtract(object message, out PartitionKey? partitionKey,
-            out ContainerInformation? containerInformation);
-
-        class Extractor : ITransactionInformationFromMessagesExtractor
+        [Test]
+        public async Task Should_pass_message_and_headers_to_container_information_extractor()
         {
-            readonly TryExtract tryExtract;
+            object capturedMessageInstanceFromContainerInformationExtractor = null;
+            IReadOnlyDictionary<string, string> capturedHeadersFromContainerInformationExtractor = null;
+            var containerInformationExtractor = new ContainerInformationExtractor(
+                (object msg, IReadOnlyDictionary<string, string> headers,
+                    out ContainerInformation? containerInformation) =>
+                {
+                    containerInformation = null;
+                    capturedMessageInstanceFromContainerInformationExtractor = msg;
+                    capturedHeadersFromContainerInformationExtractor = headers;
+                    return true;
+                });
 
-            public Extractor(TryExtract tryExtract) => this.tryExtract = tryExtract;
+            var behavior = new TransactionInformationBeforeTheLogicalOutboxBehavior(new PartitionKeyExtractor(), containerInformationExtractor);
 
-            public bool TryExtract(object message, out PartitionKey? partitionKey,
-                out ContainerInformation? containerInformation) =>
-                tryExtract(message, out partitionKey, out containerInformation);
+            var messageHeaders = new Dictionary<string, string> { { "HeaderKey", "HeaderValue" } };
+            var messageInstance = new object();
+            var context = new TestableIncomingLogicalMessageContext
+            {
+                MessageHeaders = messageHeaders,
+                Message = new LogicalMessage(new MessageMetadata(typeof(object)), messageInstance)
+            };
+
+            await behavior.Invoke(context, _ => Task.CompletedTask);
+
+            Assert.That(capturedMessageInstanceFromContainerInformationExtractor, Is.Not.Null.And.EqualTo(messageInstance));
+            Assert.That(capturedHeadersFromContainerInformationExtractor, Is.Not.Null.And.EqualTo(messageHeaders));
+        }
+
+        delegate bool TryExtractPartitionKey(object message, IReadOnlyDictionary<string, string> headers, out PartitionKey? partitionKey);
+
+        class PartitionKeyExtractor : IPartitionKeyFromMessagesExtractor
+        {
+            readonly TryExtractPartitionKey tryExtract;
+
+            public PartitionKeyExtractor(TryExtractPartitionKey tryExtract = default)
+            {
+                if (tryExtract == null)
+                {
+                    this.tryExtract = (object msg, IReadOnlyDictionary<string, string> headers,
+                        out PartitionKey? partitionKey) =>
+                    {
+                        partitionKey = null;
+                        return false;
+                    };
+                    return;
+                }
+
+                this.tryExtract = tryExtract;
+            }
+
+            public bool TryExtract(object message, IReadOnlyDictionary<string, string> headers, out PartitionKey? partitionKey) =>
+                tryExtract(message, headers, out partitionKey);
+        }
+
+        delegate bool TryExtractContainerInformation(object message, IReadOnlyDictionary<string, string> headers, out ContainerInformation? containerInformation);
+
+        class ContainerInformationExtractor : IContainerInformationFromMessagesExtractor
+        {
+            readonly TryExtractContainerInformation tryExtract;
+
+            public ContainerInformationExtractor(TryExtractContainerInformation tryExtract = default)
+            {
+                if (tryExtract == null)
+                {
+                    this.tryExtract = (object msg, IReadOnlyDictionary<string, string> headers,
+                        out ContainerInformation? containerInformation) =>
+                    {
+                        containerInformation = null;
+                        return false;
+                    };
+                    return;
+                }
+
+                this.tryExtract = tryExtract;
+            }
+
+            public bool TryExtract(object message, IReadOnlyDictionary<string, string> headers, out ContainerInformation? containerInformation) =>
+                tryExtract(message, headers, out containerInformation);
         }
     }
 }
--- src/NServiceBus.Persistence.CosmosDB.Tests/Transaction/TransactionInformationBeforeThePhysicalOutboxBehaviorTests.cs
+++ src/NServiceBus.Persistence.CosmosDB.Tests/Transaction/TransactionInformationBeforeThePhysicalOutboxBehaviorTests.cs
@@ -10,38 +10,35 @@
     public class TransactionInformationBeforeThePhysicalOutboxBehaviorTests
     {
         [Test]
-        public async Task Should_not_set_partition_key_or_container_when_extractor_returns_false()
+        public async Task Should_not_set_partition_key_when_partition_key_extractor_returns_false()
         {
-            var extractor = new Extractor(
-                delegate (IReadOnlyDictionary<string, string> headers, out PartitionKey? key, out ContainerInformation? container)
+            var extractor = new PartitionKeyExtractor(
+                (IReadOnlyDictionary<string, string> headers, out PartitionKey? key) =>
                 {
                     key = null;
-                    container = null;
                     return false;
                 });
 
-            var behavior = new TransactionInformationBeforeThePhysicalOutboxBehavior(extractor);
+            var behavior = new TransactionInformationBeforeThePhysicalOutboxBehavior(extractor, new ContainerInformationExtractor());
 
             var context = new TestableTransportReceiveContext();
 
             await behavior.Invoke(context, _ => Task.CompletedTask);
 
             Assert.That(context.Extensions.TryGet<PartitionKey>(out _), Is.False);
-            Assert.That(context.Extensions.TryGet<ContainerInformation>(out _), Is.False);
         }
 
         [Test]
-        public async Task Should_set_partition_key_when_extractor_returns_true()
+        public async Task Should_set_partition_key_when_partition_key_extractor_returns_true()
         {
-            var extractor = new Extractor(
-                delegate (IReadOnlyDictionary<string, string> headers, out PartitionKey? key, out ContainerInformation? container)
+            var partitionKeyExtractor = new PartitionKeyExtractor(
+                (IReadOnlyDictionary<string, string> headers, out PartitionKey? key) =>
                 {
                     key = new PartitionKey(true);
-                    container = null;
                     return true;
                 });
 
-            var behavior = new TransactionInformationBeforeThePhysicalOutboxBehavior(extractor);
+            var behavior = new TransactionInformationBeforeThePhysicalOutboxBehavior(partitionKeyExtractor, new ContainerInformationExtractor());
 
             var context = new TestableTransportReceiveContext();
 
@@ -52,17 +49,35 @@
         }
 
         [Test]
-        public async Task Should_set_container_when_extractor_returns_true()
+        public async Task Should_not_set_container_when_container_information_extractor_returns_false()
         {
-            var extractor = new Extractor(
-                delegate (IReadOnlyDictionary<string, string> headers, out PartitionKey? key, out ContainerInformation? container)
+            var containerInformationExtractor = new ContainerInformationExtractor(
+                (IReadOnlyDictionary<string, string> headers, out ContainerInformation? container) =>
                 {
-                    key = null;
+                    container = null;
+                    return false;
+                });
+
+            var behavior = new TransactionInformationBeforeThePhysicalOutboxBehavior(new PartitionKeyExtractor(), containerInformationExtractor);
+
+            var context = new TestableTransportReceiveContext();
+
+            await behavior.Invoke(context, _ => Task.CompletedTask);
+
+            Assert.That(context.Extensions.TryGet<ContainerInformation>(out _), Is.False);
+        }
+
+        [Test]
+        public async Task Should_set_container_when_container_information_extractor_returns_true()
+        {
+            var containerInformationExtractor = new ContainerInformationExtractor(
+                (IReadOnlyDictionary<string, string> headers, out ContainerInformation? container) =>
+                {
                     container = new ContainerInformation("containerName", new PartitionKeyPath("/deep/down"));
                     return true;
                 });
 
-            var behavior = new TransactionInformationBeforeThePhysicalOutboxBehavior(extractor);
+            var behavior = new TransactionInformationBeforeThePhysicalOutboxBehavior(new CosmosDB.PartitionKeyExtractor(), containerInformationExtractor);
 
             var context = new TestableTransportReceiveContext();
 
@@ -73,41 +88,99 @@
         }
 
         [Test]
-        public async Task Should_pass_headers()
+        public async Task Should_pass_headers_to_partition_key_extractor()
         {
             IReadOnlyDictionary<string, string> capturedHeaders = null;
-            var extractor = new Extractor(
-                delegate (IReadOnlyDictionary<string, string> headers, out PartitionKey? key, out ContainerInformation? container)
+            var partitionKeyExtractor = new PartitionKeyExtractor(
+                (IReadOnlyDictionary<string, string> headers, out PartitionKey? key) =>
                 {
                     key = null;
+                    capturedHeaders = headers;
+                    return false;
+                });
+
+            var behavior = new TransactionInformationBeforeThePhysicalOutboxBehavior(partitionKeyExtractor, new ContainerInformationExtractor());
+
+            var context = new TestableTransportReceiveContext();
+            context.Message.Headers.Add("TheAnswer", "Is42");
+
+            await behavior.Invoke(context, _ => Task.CompletedTask);
+
+            Assert.That(capturedHeaders, Is.EqualTo(context.Message.Headers));
+        }
+
+        [Test]
+        public async Task Should_pass_headers_to_container_information_extractor()
+        {
+            IReadOnlyDictionary<string, string> capturedHeaders = null;
+            var containerInformationExtractor = new ContainerInformationExtractor(
+                (IReadOnlyDictionary<string, string> headers, out ContainerInformation? container) =>
+                {
                     container = null;
                     capturedHeaders = headers;
                     return false;
                 });
 
-            var behavior = new TransactionInformationBeforeThePhysicalOutboxBehavior(extractor);
+            var behavior = new TransactionInformationBeforeThePhysicalOutboxBehavior(new PartitionKeyExtractor(), containerInformationExtractor);
 
             var context = new TestableTransportReceiveContext();
-            context.Message.Headers.Clear();
             context.Message.Headers.Add("TheAnswer", "Is42");
 
             await behavior.Invoke(context, _ => Task.CompletedTask);
 
-            Assert.That(capturedHeaders, Is.Not.Null.And.ContainKey("TheAnswer").WithValue("Is42").And.Count.EqualTo(1));
+            Assert.That(capturedHeaders, Is.EqualTo(context.Message.Headers));
         }
 
-        delegate bool TryExtract(IReadOnlyDictionary<string, string> headers, out PartitionKey? partitionKey,
-            out ContainerInformation? containerInformation);
+        delegate bool TryExtractPartitionKey(IReadOnlyDictionary<string, string> headers, out PartitionKey? partitionKey);
 
-        class Extractor : ITransactionInformationFromHeadersExtractor
+        class PartitionKeyExtractor : IPartitionKeyFromHeadersExtractor
         {
-            readonly TryExtract tryExtract;
+            readonly TryExtractPartitionKey tryExtract;
 
-            public Extractor(TryExtract tryExtract) => this.tryExtract = tryExtract;
+            public PartitionKeyExtractor(TryExtractPartitionKey tryExtract = default)
+            {
+                if (tryExtract == null)
+                {
+                    this.tryExtract = (IReadOnlyDictionary<string, string> headers,
+                        out PartitionKey? partitionKey) =>
+                    {
+                        partitionKey = null;
+                        return false;
+                    };
+                    return;
+                }
 
-            public bool TryExtract(IReadOnlyDictionary<string, string> headers, out PartitionKey? partitionKey,
-                out ContainerInformation? containerInformation) =>
-                tryExtract(headers, out partitionKey, out containerInformation);
+                this.tryExtract = tryExtract;
+            }
+
+            public bool TryExtract(IReadOnlyDictionary<string, string> headers, out PartitionKey? partitionKey) =>
+                tryExtract(headers, out partitionKey);
+        }
+
+        delegate bool TryExtractContainerInformation(IReadOnlyDictionary<string, string> headers, out ContainerInformation? containerInformation);
+
+        class ContainerInformationExtractor : IContainerInformationFromHeadersExtractor
+        {
+            readonly TryExtractContainerInformation tryExtract;
+
+            public ContainerInformationExtractor(TryExtractContainerInformation tryExtract = default)
+            {
+                if (tryExtract == null)
+                {
+                    this.tryExtract = (IReadOnlyDictionary<string, string> headers,
+                        out ContainerInformation? containerInformation) =>
+                    {
+                        containerInformation = null;
+                        return false;
+                    };
+                    return;
+                }
+
+                this.tryExtract = tryExtract;
+            }
+
+            public bool TryExtract(IReadOnlyDictionary<string, string> headers, out ContainerInformation? containerInformation) =>
+                tryExtract(headers, out containerInformation);
         }
     }
 }
