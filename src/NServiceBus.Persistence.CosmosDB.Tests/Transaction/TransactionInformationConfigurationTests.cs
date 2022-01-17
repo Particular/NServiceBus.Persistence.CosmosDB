535d6108be615b91b7d593c206a241d31eb2ce4f
danielmarbach <daniel.marbach@openplace.net>
2022-01-17 12:10:11 +0100

    Make sure we don't miss exposing extraction APIs

---
+++ src/NServiceBus.Persistence.CosmosDB.Tests/Transaction/TransactionInformationConfigurationTests.cs
@@ -0,0 +1,45 @@
+namespace NServiceBus.Persistence.CosmosDB.Tests.Transaction
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using NUnit.Framework;
+    using Particular.Approvals;
+
+    [TestFixture]
+    public class TransactionInformationConfigurationTests
+    {
+        [Test]
+        public void Should_have_all_relevant_extraction_apis_exposed()
+        {
+            var transactionInformationConfigurationMethods = typeof(TransactionInformationConfiguration)
+                .GetMethods(BindingFlags.Public | BindingFlags.Instance)
+                .Where(m => m.Name.StartsWith("Extract"))
+                .OrderBy(m => m.Name)
+                .ThenBy(m => m.GetParameters().Length)
+                .Select(m => m.ToString());
+
+            var containerInformationExtractorMethods = typeof(ContainerInformationExtractor)
+                .GetMethods(BindingFlags.Public | BindingFlags.Instance)
+                .Where(m => m.Name.StartsWith("Extract"))
+                .OrderBy(m => m.Name)
+                .ThenBy(m => m.GetParameters().Length)
+                .Select(m => m.ToString());
+
+            var partitionKeyExtractorMethods = typeof(PartitionKeyExtractor)
+                .GetMethods(BindingFlags.Public | BindingFlags.Instance)
+                .Where(m => m.Name.StartsWith("Extract"))
+                .OrderBy(m => m.Name)
+                .ThenBy(m => m.GetParameters().Length)
+                .Select(m => m.ToString());
+
+            // represents the missing extraction methods in the TransactionInformationConfiguration object
+            var methodInfos = containerInformationExtractorMethods.Union(partitionKeyExtractorMethods).Except(transactionInformationConfigurationMethods)
+                .Distinct()
+                .ToArray();
+
+            Approver.Verify(string.Join(Environment.NewLine, methodInfos));
+        }
+    }
+}
