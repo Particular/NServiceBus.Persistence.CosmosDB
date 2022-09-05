42d5c75d682343d30d84bba6ebb8ad1d086aadcd
danielmarbach <daniel.marbach@openplace.net>
2022-09-05 11:36:59 +0200

    Cleanup feature

--- src/NServiceBus.Persistence.CosmosDB.TransactionalSession/CosmosTransactionalSessionExtensions.cs
+++ src/NServiceBus.Persistence.CosmosDB.TransactionalSession/CosmosTransactionalSessionExtensions.cs
@@ -1,24 +1,21 @@
-﻿namespace NServiceBus.TransactionalSession;
-
-using Configuration.AdvancedExtensibility;
-using Features;
-
-/// <summary>
-/// Enables the transactional session feature.
-/// </summary>
-public static class CosmosTransactionalSessionExtensions
+﻿namespace NServiceBus.TransactionalSession
 {
-    /// <summary>
-    /// Enables transactional session for this endpoint.
-    /// </summary>
-    /// <param name="persistenceExtensions"></param>
-    /// <returns></returns>
-    public static PersistenceExtensions<CosmosPersistence> EnableTransactionalSession(
-        this PersistenceExtensions<CosmosPersistence> persistenceExtensions)
-    {
-        persistenceExtensions.GetSettings().EnableFeatureByDefault(typeof(TransactionalSession));
-        persistenceExtensions.GetSettings().EnableFeatureByDefault(typeof(CosmosTransactionalSession));
+    using Configuration.AdvancedExtensibility;
+    using Features;
 
-        return persistenceExtensions;
+    /// <summary>
+    /// Enables the transactional session feature.
+    /// </summary>
+    public static class CosmosTransactionalSessionExtensions
+    {
+        /// <summary>
+        /// Enables transactional session for this endpoint.
+        /// </summary>
+        public static PersistenceExtensions<CosmosPersistence> EnableTransactionalSession(
+            this PersistenceExtensions<CosmosPersistence> persistenceExtensions)
+        {
+            persistenceExtensions.GetSettings().EnableFeatureByDefault<CosmosTransactionalSession>();
+            return persistenceExtensions;
+        }
     }
 }
