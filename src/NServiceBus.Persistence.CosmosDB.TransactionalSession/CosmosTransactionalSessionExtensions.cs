d35ad22be8aae2477576a85b855bf47a7565d4d5
Daniel Marbach <daniel.marbach@openplace.net>
2022-09-07 11:54:30 +0200

    Implement TransactionalSession as part of the persistence (#417)
    * Implement TransactionalSession as part of the persistence
    
    Co-authored-by: Tomasz Masternak <tomasz.masternak@particular.net>
    
    * persister specific extension method to enable tx session
    
    * Cleanup feature
    
    * Api Approvals adjustment
    
    * Transactional Session API Approvals
    
    * Add transactional session to scanning too
    
    * Adjust to make the tx session atts its own concept
    
    * Remove no longer required internals visible
    
    * Get rid of unnecessary file
    
    * Approve
    
    * Use scenario testrun id as partition key
    
    * Remove redundant call
    
    * Adjust to package updates
    
    * Reference the package explicitely
    
    * Bump RC3
    
    Co-authored-by: Tomasz Masternak <tomasz.masternak@particular.net>

---
+++ src/NServiceBus.Persistence.CosmosDB.TransactionalSession/CosmosTransactionalSessionExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace NServiceBus.TransactionalSession
+{
+    using Configuration.AdvancedExtensibility;
+    using Features;
+
+    /// <summary>
+    /// Enables the transactional session feature.
+    /// </summary>
+    public static class CosmosTransactionalSessionExtensions
+    {
+        /// <summary>
+        /// Enables transactional session for this endpoint.
+        /// </summary>
+        public static PersistenceExtensions<CosmosPersistence> EnableTransactionalSession(
+            this PersistenceExtensions<CosmosPersistence> persistenceExtensions)
+        {
+            persistenceExtensions.GetSettings().EnableFeatureByDefault<CosmosTransactionalSession>();
+            return persistenceExtensions;
+        }
+    }
+}
