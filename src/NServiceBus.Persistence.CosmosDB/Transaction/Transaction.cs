0e733f7647ae078b46bc5de3cf6c3b7ba38706f8
danielmarbach <daniel.marbach@openplace.net>
2022-09-12 13:44:33 +0200

    Set TransactionInformationConfiguration as defaults
    (cherry picked from commit 91ebe7232bd5905d9d07090b27a3a81e3b0cb559)

--- src/NServiceBus.Persistence.CosmosDB/Transaction/Transaction.cs
+++ src/NServiceBus.Persistence.CosmosDB/Transaction/Transaction.cs
@@ -2,11 +2,13 @@ namespace NServiceBus.Persistence.CosmosDB
 {
     using Features;
 
-    class Transaction : Feature
+    sealed class Transaction : Feature
     {
+        public Transaction() => Defaults(s => s.SetDefault(new TransactionInformationConfiguration()));
+
         protected override void Setup(FeatureConfigurationContext context)
         {
-            var configuration = context.Settings.GetOrDefault<TransactionInformationConfiguration>() ?? new TransactionInformationConfiguration();
+            var configuration = context.Settings.Get<TransactionInformationConfiguration>();
 
             context.Pipeline.Register(new TransactionInformationBeforeTheLogicalOutboxBehavior.RegisterStep(configuration.PartitionKeyExtractor, configuration.ContainerInformationExtractor));
             context.Pipeline.Register(new TransactionInformationBeforeThePhysicalOutboxBehavior.RegisterStep(configuration.PartitionKeyExtractor, configuration.ContainerInformationExtractor));
