905b13e1be8ab64c5abd9437e55d909680b97537
danielmarbach <daniel.marbach@openplace.net>
2024-06-26 17:44:31 +0200

    Add test that verifies the default credentials work

---
+++ src/SharedAcceptanceTests.All/When_default_credentials_used.cs
@@ -0,0 +1,79 @@
+﻿namespace NServiceBus.AcceptanceTests
+{
+    using System;
+    using System.Data.Common;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using Azure.Identity;
+    using EndpointTemplates;
+    using Microsoft.Azure.Cosmos;
+    using NUnit.Framework;
+
+    public class When_default_credentials_used : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_work()
+        {
+            var context = await Scenario.Define<Context>()
+                .WithEndpoint<EndpointWithCustomProvider>(b => b.When(session => session.SendLocal(new StartSaga1
+                {
+                    DataId = Guid.NewGuid()
+                })))
+                .Done(c => c.SagaReceivedMessage)
+                .Run();
+
+            Assert.True(context.ProviderWasCalled);
+        }
+
+        public class Context : ScenarioContext
+        {
+            public bool SagaReceivedMessage { get; set; }
+            public bool ProviderWasCalled { get; set; }
+        }
+
+        public class EndpointWithCustomProvider : EndpointConfigurationBuilder
+        {
+            public EndpointWithCustomProvider() =>
+                EndpointSetup<DefaultServer>(config =>
+                {
+                    var builder = new DbConnectionStringBuilder
+                    {
+                        ConnectionString = SetupFixture.GetConnectionStringOrFallback()
+                    };
+                    builder.TryGetValue("AccountEndpoint", out var accountEndpoint);
+
+                    var cosmosClient = new CosmosClient($"{accountEndpoint}", new DefaultAzureCredential(), new CosmosClientOptions());
+
+                    config.UsePersistence<CosmosPersistence>().CosmosClient(cosmosClient);
+                });
+
+            public class JustASaga : Saga<JustASagaData>, IAmStartedByMessages<StartSaga1>
+            {
+                public JustASaga(Context testContext) => this.testContext = testContext;
+
+                public Task Handle(StartSaga1 message, IMessageHandlerContext context)
+                {
+                    Data.DataId = message.DataId;
+                    testContext.SagaReceivedMessage = true;
+                    MarkAsComplete();
+                    return Task.CompletedTask;
+                }
+
+                protected override void ConfigureHowToFindSaga(SagaPropertyMapper<JustASagaData> mapper)
+                    => mapper.ConfigureMapping<StartSaga1>(m => m.DataId).ToSaga(s => s.DataId);
+
+                readonly Context testContext;
+            }
+
+            public class JustASagaData : ContainSagaData
+            {
+                public virtual Guid DataId { get; set; }
+            }
+        }
+
+        public class StartSaga1 : ICommand
+        {
+            public Guid DataId { get; set; }
+        }
+    }
+}
