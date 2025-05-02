77beb99e72d7ae14d1d00f8b264b9a98f33b9417
Andreas Öhlund <andreas.ohlund@particular.net>
2025-05-02 08:50:07 +0200

    Separate file

--- src/NServiceBus.Persistence.CosmosDB.TransactionalSession.AcceptanceTests/Infrastructure/TransactionSessionDefaultServer.cs
+++ src/NServiceBus.Persistence.CosmosDB.TransactionalSession.AcceptanceTests/Infrastructure/TransactionSessionDefaultServer.cs
@@ -1,17 +1,9 @@
 namespace NServiceBus.TransactionalSession.AcceptanceTests;
 
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Threading.Tasks;
-using AcceptanceTesting;
-using AcceptanceTesting.Customization;
 using AcceptanceTesting.Support;
 using Configuration.AdvancedExtensibility;
-using Microsoft.Azure.Cosmos;
-using Microsoft.Extensions.DependencyInjection;
-using NUnit.Framework;
-using Persistence.CosmosDB;
 
 public class TransactionSessionDefaultServer : DefaultServer
 {
@@ -23,60 +15,6 @@ public class TransactionSessionDefaultServer : DefaultServer
             .EnableTransactionalSession();
 
         return endpointConfiguration;
-    }
-}
-
-public class DefaultServer : IEndpointSetupTemplate
-{
-    public virtual async Task<EndpointConfiguration> GetConfiguration(RunDescriptor runDescriptor, EndpointCustomizationConfiguration endpointCustomization,
-        Func<EndpointConfiguration, Task> configurationBuilderCustomization)
-    {
-        var endpointConfiguration = new EndpointConfiguration(endpointCustomization.EndpointName);
-
-        endpointConfiguration.EnableInstallers();
-        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
-
-        endpointConfiguration.Recoverability()
-            .Delayed(delayed => delayed.NumberOfRetries(0))
-            .Immediate(immediate => immediate.NumberOfRetries(0));
-        endpointConfiguration.SendFailedMessagesTo("error");
-
-        string storageDir = Path.Combine(Path.GetTempPath(), "learn", TestContext.CurrentContext.Test.ID);
-
-        endpointConfiguration.UseTransport(new AcceptanceTestingTransport { StorageLocation = storageDir });
-
-        var persistence = endpointConfiguration.UsePersistence<CosmosPersistence>();
-
-        endpointConfiguration.GetSettings().Set(persistence);
-        persistence.EnableTransactionalSession();
-        persistence.DisableContainerCreation();
-        persistence.CosmosClient(SetupFixture.CosmosDbClient);
-        persistence.DatabaseName(SetupFixture.DatabaseName);
-
-        persistence.DefaultContainer(SetupFixture.ContainerName, SetupFixture.PartitionPathKey);
-
-        endpointConfiguration.RegisterComponents(services => services.AddSingleton<IPartitionKeyFromHeadersExtractor, PartitionKeyProvider>());
-
-        if (this is not IDoNotCaptureServiceProvider)
-        {
-            endpointConfiguration.RegisterStartupTask(sp => new CaptureServiceProviderStartupTask(sp, runDescriptor.ScenarioContext));
-        }
-
-        await configurationBuilderCustomization(endpointConfiguration).ConfigureAwait(false);
-
-        // scan types at the end so that all types used by the configuration have been loaded into the AppDomain
-        endpointConfiguration.TypesToIncludeInScan(endpointCustomization.GetTypesScopedByTestClass());
-
-        return endpointConfiguration;
-    }
-
-    class PartitionKeyProvider(ScenarioContext scenarioContext) : IPartitionKeyFromHeadersExtractor
-    {
-        public bool TryExtract(IReadOnlyDictionary<string, string> headers, out PartitionKey? partitionKey)
-        {
-            partitionKey = new PartitionKey(scenarioContext.TestRunId.ToString());
-            return true;
-        }
     }
 }
 
