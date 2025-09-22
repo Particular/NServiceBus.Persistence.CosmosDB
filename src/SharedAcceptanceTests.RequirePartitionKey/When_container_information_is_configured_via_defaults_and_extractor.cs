7f4b75b6e1747b9a6f7593c8de54e815e4de0e26
Jo Palac <jo.palac@particular.net>
2025-09-22 11:59:28 +1000

    add test for overwriting default container with extractor

---
+++ src/SharedAcceptanceTests.RequirePartitionKey/When_container_information_is_configured_via_defaults_and_extractor.cs
@@ -0,0 +1,71 @@
+namespace NServiceBus.AcceptanceTests;
+
+using System.Threading.Tasks;
+using AcceptanceTesting;
+using EndpointTemplates;
+using Microsoft.Azure.Cosmos;
+using NServiceBus.AcceptanceTesting.Support;
+using NUnit.Framework;
+
+[TestFixture]
+public class When_container_information_is_configured_via_defaults_and_extractor : NServiceBusAcceptanceTest
+{
+    static string defaultContainerName = "testContainer";
+
+    [Test]
+    public async Task Should_overwrite_default_with_extractor_container()
+    {
+        var runSettings = new RunSettings();
+        runSettings.DoNotRegisterDefaultPartitionKeyProvider();
+
+        Context context = await Scenario.Define<Context>()
+            .WithEndpoint<Endpoint>(b =>
+            {
+                b.When(s => s.SendLocal(new MyMessage()));
+            })
+            .Done(c => c.Done)
+            .Run(runSettings);
+
+        Assert.That(context.Container.Id, Is.EqualTo(SetupFixture.ContainerName));
+    }
+
+    class Context : ScenarioContext
+    {
+        public bool Done { get; set; }
+        public Container Container { get; set; }
+    }
+
+    class Endpoint : EndpointConfigurationBuilder
+    {
+        public Endpoint() =>
+            EndpointSetup<DefaultServer>((config, runDescriptor) =>
+            {
+                config.EnableOutbox();
+                config.ConfigureTransport().TransportTransactionMode = TransportTransactionMode.ReceiveOnly;
+                PersistenceExtensions<CosmosPersistence> persistence = config.UsePersistence<CosmosPersistence>();
+                persistence.DefaultContainer(defaultContainerName, SetupFixture.PartitionPathKey);
+            });
+
+        class MyMessageHandler : IHandleMessages<MyMessage>
+        {
+            public MyMessageHandler(ICosmosStorageSession session, Context context)
+            {
+                this.session = session;
+                this.context = context;
+            }
+            public Task Handle(MyMessage message, IMessageHandlerContext handlerContext)
+            {
+                context.Done = true;
+                context.Container = session.Container;
+                return Task.CompletedTask;
+            }
+
+            Context context;
+            ICosmosStorageSession session;
+        }
+    }
+
+    class MyMessage : IMessage
+    {
+    }
+}
