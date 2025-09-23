e47e3718670b01f5354ef9fc95b50c2d682a01a4
Jo Palac <jo.palac@particular.net>
2025-09-23 14:45:59 +1000

    add tests for configuring both container extractors

---
+++ src/NServiceBus.Persistence.CosmosDB.PhysicalOutbox.AcceptanceTests/When_no_default_container_and_both_container_extractors_configured.cs
@@ -0,0 +1,130 @@
+﻿namespace NServiceBus.AcceptanceTests;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AcceptanceTesting;
+using AcceptanceTesting.Support;
+using EndpointTemplates;
+using Microsoft.Azure.Cosmos;
+using NUnit.Framework;
+using Persistence.CosmosDB;
+
+public class When_no_default_container_and_both_container_extractors_configured : NServiceBusAcceptanceTest
+{
+    static string messageContainerName = $"{SetupFixture.ContainerName}_message";
+    Container messageContainer;
+
+    [SetUp]
+    public async Task Setup()
+    {
+        await SetupFixture.CosmosDbClient.CreateDatabaseIfNotExistsAsync(SetupFixture.DatabaseName)
+            .ConfigureAwait(false);
+
+        Database database = SetupFixture.CosmosDbClient.GetDatabase(SetupFixture.DatabaseName);
+
+        var messageContainerProperties =
+            new ContainerProperties(messageContainerName, SetupFixture.PartitionPathKey)
+            {
+                // in order for individual items TTL to work (example outbox records)
+                DefaultTimeToLive = -1
+            };
+
+        await database.CreateContainerIfNotExistsAsync(messageContainerProperties)
+            .ConfigureAwait(false);
+
+        messageContainer = database.GetContainer(messageContainerName);
+    }
+
+    [TearDown]
+    public async Task Teardown()
+    {
+        await messageContainer.DeleteContainerStreamAsync();
+    }
+
+    [Test]
+    public async Task Header_extractor_should_be_used()
+    {
+        var runSettings = new RunSettings();
+        runSettings.DoNotRegisterDefaultContainerInformationProvider();
+        runSettings.DoNotRegisterDefaultPartitionKeyProvider();
+
+        Context context = await Scenario.Define<Context>()
+            .WithEndpoint<EndpointWithCustomExtractors>(b =>
+            {
+                b.When(s => s.SendLocal(new MyMessage()));
+            })
+            .Done(c => c.Done)
+            .Run(runSettings);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(context.HeaderExtractorWasCalled, Is.True);
+            Assert.That(context.Container.Id, Is.EqualTo(SetupFixture.ContainerName));
+        });
+    }
+
+    public class Context : ScenarioContext
+    {
+        public bool Done { get; set; }
+        public Container Container { get; set; }
+        public bool MessageExtractorWasCalled { get; set; }
+        public bool HeaderExtractorWasCalled { get; set; }
+    }
+
+    public class EndpointWithCustomExtractors : EndpointConfigurationBuilder
+    {
+        public EndpointWithCustomExtractors() =>
+            EndpointSetup<DefaultServer>((config, r) =>
+            {
+                config.EnableOutbox();
+                config.ConfigureTransport().TransportTransactionMode = TransportTransactionMode.ReceiveOnly;
+                PersistenceExtensions<CosmosPersistence> persistence = config.UsePersistence<CosmosPersistence>();
+                TransactionInformationConfiguration transactionInformation = persistence.TransactionInformation();
+                transactionInformation.ExtractContainerInformationFromMessage(new CustomMessageExtractor((Context)r.ScenarioContext));
+                transactionInformation.ExtractContainerInformationFromHeaders(new CustomHeadersExtractor((Context)r.ScenarioContext));
+            });
+
+        class MyMessageHandler : IHandleMessages<MyMessage>
+        {
+            public MyMessageHandler(ICosmosStorageSession session, Context context)
+            {
+                this.session = session;
+                this.context = context;
+            }
+            public Task Handle(MyMessage message, IMessageHandlerContext handlerContext)
+            {
+                context.Done = true;
+                context.Container = session.Container;
+                return Task.CompletedTask;
+            }
+
+            Context context;
+            ICosmosStorageSession session;
+        }
+
+        public class CustomMessageExtractor(Context testContext) : IContainerInformationFromMessagesExtractor
+        {
+            public bool TryExtract(object message, IReadOnlyDictionary<string, string> headers, out ContainerInformation? containerInformation)
+            {
+                containerInformation = new ContainerInformation(messageContainerName, new PartitionKeyPath(SetupFixture.PartitionPathKey));
+                testContext.MessageExtractorWasCalled = true;
+                return true;
+            }
+        }
+
+        public class CustomHeadersExtractor(Context testContext) : IContainerInformationFromHeadersExtractor
+        {
+            public bool TryExtract(IReadOnlyDictionary<string, string> headers, out ContainerInformation? containerInformation)
+            {
+                containerInformation = new ContainerInformation(SetupFixture.ContainerName, new PartitionKeyPath(SetupFixture.PartitionPathKey));
+                testContext.HeaderExtractorWasCalled = true;
+                return true;
+            }
+        }
+
+    }
+
+    class MyMessage : IMessage
+    {
+    }
+}
