f0038538d3e3b823db82de56b603cf04ed093a4f
Brandon Ording <bording@gmail.com>
2025-10-03 11:32:53 -0400

    Remove XML comments in obsoletes file

--- src/NServiceBus.Persistence.CosmosDB/obsoletes-v4.cs
+++ src/NServiceBus.Persistence.CosmosDB/obsoletes-v4.cs
@@ -1,13 +1,12 @@
-﻿namespace NServiceBus;
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace NServiceBus;
 
 using System;
 using Particular.Obsoletes;
 
 public static partial class CosmosPersistenceConfig
 {
-    /// <summary>
-    /// Enables using extracted container information from the incoming message. Without this setting, only extracted container information from incoming message headers will be used
-    /// </summary>
     [ObsoleteMetadata(
             Message = "Using extracted container information from the incoming message will become the default behavior starting in version 4.0, making this API redundant",
             RemoveInVersion = "5",
