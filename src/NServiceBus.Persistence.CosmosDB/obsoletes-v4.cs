d16d3e0167eb0a94a9e44d22d2f5a1a1ddd66770
Brandon Ording <bording@gmail.com>
2025-10-03 11:31:29 -0400

    Update obsolete message

--- src/NServiceBus.Persistence.CosmosDB/obsoletes-v4.cs
+++ src/NServiceBus.Persistence.CosmosDB/obsoletes-v4.cs
@@ -9,9 +9,9 @@ public static partial class CosmosPersistenceConfig
     /// Enables using extracted container information from the incoming message. Without this setting, only extracted container information from incoming message headers will be used
     /// </summary>
     [ObsoleteMetadata(
-            Message = "The EnableContainerFromMessageExtractor will become default behavior from v4.0",
+            Message = "Using extracted container information from the incoming message will become the default behavior starting in version 4.0, making this API redundant",
             RemoveInVersion = "5",
             TreatAsErrorFromVersion = "4")]
-    [Obsolete("The EnableContainerFromMessageExtractor will become default behavior from v4.0. Will be removed in version 5.0.0.", true)]
+    [Obsolete("Using extracted container information from the incoming message will become the default behavior starting in version 4.0, making this API redundant. Will be removed in version 5.0.0.", true)]
     public static PersistenceExtensions<CosmosPersistence> EnableContainerFromMessageExtractor(this PersistenceExtensions<CosmosPersistence> persistenceExtensions) => throw new NotImplementedException();
 }
