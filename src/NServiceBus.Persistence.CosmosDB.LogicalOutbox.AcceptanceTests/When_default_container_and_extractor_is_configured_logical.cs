66ae1bf557d4b88452b6866fef3c2e3fa588171c
Phil Bastian <phil.bastian@particular.net>
2025-10-06 10:32:26 +0800

    only run negative case when running logical tests

---
+++ src/NServiceBus.Persistence.CosmosDB.LogicalOutbox.AcceptanceTests/When_default_container_and_extractor_is_configured_logical.cs
@@ -0,0 +1,27 @@
+﻿namespace NServiceBus.AcceptanceTests;
+
+using System.Threading.Tasks;
+using AcceptanceTesting;
+using NServiceBus.AcceptanceTesting.Support;
+using NUnit.Framework;
+
+[TestFixture]
+public partial class When_default_container_and_extractor_is_configured : NServiceBusAcceptanceTest
+{
+    [Test]
+    public async Task Should_not_overwrite_default_with_extractor_container()
+    {
+        var runSettings = new RunSettings();
+        runSettings.DoNotRegisterDefaultPartitionKeyProvider();
+
+        Context context = await Scenario.Define<Context>()
+            .WithEndpoint(new Endpoint(false), b =>
+            {
+                b.When(s => s.SendLocal(new MyMessage()));
+            })
+            .Done(c => c.Done)
+            .Run(runSettings);
+
+        Assert.That(context.Container.Id, Is.EqualTo(defaultContainerName));
+    }
+}
--- src/SharedAcceptanceTests.RequirePartitionKey/When_default_container_and_extractor_is_configured.cs
+++ src/SharedAcceptanceTests.RequirePartitionKey/When_default_container_and_extractor_is_configured.cs
@@ -8,7 +8,7 @@ using NServiceBus.AcceptanceTesting.Support;
 using NUnit.Framework;
 
 [TestFixture]
-public class When_default_container_and_extractor_is_configured : NServiceBusAcceptanceTest
+public partial class When_default_container_and_extractor_is_configured : NServiceBusAcceptanceTest
 {
     static string defaultContainerName = $"{SetupFixture.ContainerName}_default";
     Container defaultContainer;
@@ -55,23 +55,6 @@ public class When_default_container_and_extractor_is_configured : NServiceBusAcc
             .Run(runSettings);
 
         Assert.That(context.Container.Id, Is.EqualTo(SetupFixture.ContainerName));
-    }
-
-    [Test]
-    public async Task Should_not_overwrite_default_with_extractor_container()
-    {
-        var runSettings = new RunSettings();
-        runSettings.DoNotRegisterDefaultPartitionKeyProvider();
-
-        Context context = await Scenario.Define<Context>()
-            .WithEndpoint(new Endpoint(false), b =>
-            {
-                b.When(s => s.SendLocal(new MyMessage()));
-            })
-            .Done(c => c.Done)
-            .Run(runSettings);
-
-        Assert.That(context.Container.Id, Is.EqualTo(defaultContainerName));
     }
 
     class Context : ScenarioContext
