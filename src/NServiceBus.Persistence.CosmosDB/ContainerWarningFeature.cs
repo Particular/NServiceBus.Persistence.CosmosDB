0509b630d93f5c8149122ffe571fd293f5676c0d
Phil Bastian <phil.bastian@particular.net>
2025-10-07 12:06:34 +0800

    include a warning if it's likely that EnableContainerFromMessageExtractor should be set but isn't

---
+++ src/NServiceBus.Persistence.CosmosDB/ContainerWarningFeature.cs
@@ -0,0 +1,26 @@
+﻿namespace NServiceBus;
+
+using NServiceBus.Features;
+using NServiceBus.Logging;
+
+class ContainerWarningFeature : Feature
+{
+    public ContainerWarningFeature()
+    {
+        EnableByDefault();
+    }
+
+    protected override void Setup(FeatureConfigurationContext context)
+    {
+        var hasDefaultContainer = context.Settings.TryGet(out ContainerInformation _);
+        var persistence = context.Settings.Get<PersistenceExtensions<CosmosPersistence>>();
+        var hasSetNewBehaviour = context.Settings.GetOrDefault<bool>(CosmosPersistenceConfig.EnableContainerFromMessageExtractorKey);
+
+        if (persistence.TransactionInformation().HasCustomContainerMessageExtractors && hasDefaultContainer && !hasSetNewBehaviour)
+        {
+            log.Warn("The current endpoint setup has both default container and message container extractors configured, but does not have `EnableContainerFromMessageExtractor` set.");
+        }
+    }
+
+    static readonly ILog log = LogManager.GetLogger<ContainerWarningFeature>();
+}
