2ee6513125aa8798a0fc006243be48f8b5e81c61
Warwick Schroeder <warwick.schroeder@particular.net>
2025-10-08 12:41:32 +0800

    Fix versions

--- src/NServiceBus.Persistence.CosmosDB.Analyzers/NServiceBus.Persistence.CosmosDB.Analyzers/ContainerExtractorConfigurationAnalyzer.cs
+++ src/NServiceBus.Persistence.CosmosDB.Analyzers/NServiceBus.Persistence.CosmosDB.Analyzers/ContainerExtractorConfigurationAnalyzer.cs
@@ -1,116 +1,115 @@
-﻿namespace NServiceBus.Persistence.CosmosDB.Analyzers;
-
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Diagnostics;
-using System.Collections.Immutable;
-using System.Linq;
-
-[DiagnosticAnalyzer(LanguageNames.CSharp)]
-public class ContainerExtractorConfigurationAnalyzer : DiagnosticAnalyzer
+﻿namespace NServiceBus.Persistence.CosmosDB.Analyzers
 {
-    public static readonly DiagnosticDescriptor MissingEnableContainerFromMessageExtractor = new DiagnosticDescriptor(
-        "NSBC001",
-        "EnableContainerFromMessageExtractor should be called when using both default container and message extractors",
-        "The endpoint has both default container and message container extractors configured, but does not have EnableContainerFromMessageExtractor set. Consider calling persistence.EnableContainerFromMessageExtractor().",
-        "Configuration",
-        DiagnosticSeverity.Warning,
-        isEnabledByDefault: true,
-        description: "When configuring both a default container and container message extractors, you should call EnableContainerFromMessageExtractor to ensure the message extractors take precedence.");
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using System.Collections.Immutable;
+    using System.Linq;
 
-    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics =>
-        ImmutableArray.Create(MissingEnableContainerFromMessageExtractor);
-
-    public override void Initialize(AnalysisContext context)
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ContainerExtractorConfigurationAnalyzer : DiagnosticAnalyzer
     {
-        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
-        context.EnableConcurrentExecution();
-        context.RegisterSyntaxNodeAction(AnalyzeMethodDeclaration, SyntaxKind.MethodDeclaration);
-        context.RegisterSyntaxNodeAction(AnalyzeConstructorDeclaration, SyntaxKind.ConstructorDeclaration);
-    }
+        public static readonly DiagnosticDescriptor MissingEnableContainerFromMessageExtractor = new DiagnosticDescriptor(
+            "NSBC001",
+            "EnableContainerFromMessageExtractor should be called when using both default container and message extractors",
+            "The endpoint has both default container and message container extractors configured, but does not have EnableContainerFromMessageExtractor set. Consider calling persistence.EnableContainerFromMessageExtractor().",
+            "Configuration",
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
+            description: "When configuring both a default container and container message extractors, you should call EnableContainerFromMessageExtractor to ensure the message extractors take precedence.");
 
-    static void AnalyzeMethodDeclaration(SyntaxNodeAnalysisContext context)
-    {
-        var methodDeclaration = (MethodDeclarationSyntax)context.Node;
-        AnalyzeForContainerConfiguration(context, methodDeclaration.Body);
-    }
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics =>
+            ImmutableArray.Create(MissingEnableContainerFromMessageExtractor);
 
-    static void AnalyzeConstructorDeclaration(SyntaxNodeAnalysisContext context)
-    {
-        var constructorDeclaration = (ConstructorDeclarationSyntax)context.Node;
-        AnalyzeForContainerConfiguration(context, constructorDeclaration.Body);
-    }
-
-    static void AnalyzeForContainerConfiguration(SyntaxNodeAnalysisContext context, BlockSyntax body)
-    {
-        if (body == null)
+        public override void Initialize(AnalysisContext context)
         {
-            return;
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(AnalyzeMethodDeclaration, SyntaxKind.MethodDeclaration);
+            context.RegisterSyntaxNodeAction(AnalyzeConstructorDeclaration, SyntaxKind.ConstructorDeclaration);
         }
 
-        var statements = body.Statements;
-
-        var hasDefaultContainer = false;
-        var hasContainerExtractor = false;
-        var hasEnableContainerFromMessageExtractor = false;
-
-        foreach (var statement in statements)
+        static void AnalyzeMethodDeclaration(SyntaxNodeAnalysisContext context)
         {
-            var expressionStatements = statement.DescendantNodesAndSelf()
-                .OfType<InvocationExpressionSyntax>();
+            var methodDeclaration = (MethodDeclarationSyntax)context.Node;
+            AnalyzeForContainerConfiguration(context, methodDeclaration.Body);
+        }
 
-            foreach (var invocation in expressionStatements)
+        static void AnalyzeConstructorDeclaration(SyntaxNodeAnalysisContext context)
+        {
+            var constructorDeclaration = (ConstructorDeclarationSyntax)context.Node;
+            AnalyzeForContainerConfiguration(context, constructorDeclaration.Body);
+        }
+
+        static void AnalyzeForContainerConfiguration(SyntaxNodeAnalysisContext context, BlockSyntax body)
+        {
+            if (body == null)
             {
-                if (invocation.Expression is not MemberAccessExpressionSyntax memberAccess)
-                {
-                    continue;
-                }
+                return;
+            }
 
-                var methodName = memberAccess.Name.Identifier.ValueText;
+            var statements = body.Statements;
 
-                // Check for DefaultContainer call on the correct type
-                if (methodName == "DefaultContainer")
+            var hasDefaultContainer = false;
+            var hasContainerExtractor = false;
+            var hasEnableContainerFromMessageExtractor = false;
+
+            foreach (var statement in statements)
+            {
+                var expressionStatements = statement.DescendantNodesAndSelf()
+                    .OfType<InvocationExpressionSyntax>();
+
+                foreach (var invocation in expressionStatements)
                 {
-                    if (IsCosmosPersistenceExtensionMethod(context, memberAccess))
+                    if (invocation.Expression is MemberAccessExpressionSyntax memberAccess)
                     {
-                        hasDefaultContainer = true;
-                    }
-                }
-                // Check for container extractor methods
-                else if (methodName == "ExtractContainerInformationFromMessage")
-                {
-                    hasContainerExtractor = true;
-                }
-                // Check for EnableContainerFromMessageExtractor call
-                else if (methodName == "EnableContainerFromMessageExtractor")
-                {
-                    if (IsCosmosPersistenceExtensionMethod(context, memberAccess))
-                    {
-                        hasEnableContainerFromMessageExtractor = true;
+                        var methodName = memberAccess.Name.Identifier.ValueText;
+
+                        // Check for DefaultContainer call on the correct type
+                        if (methodName == "DefaultContainer")
+                        {
+                            if (IsCosmosPersistenceExtensionMethod(context, memberAccess))
+                            {
+                                hasDefaultContainer = true;
+                            }
+                        }
+                        // Check for container extractor methods
+                        else if (methodName == "ExtractContainerInformationFromMessage")
+                        {
+                            hasContainerExtractor = true;
+                        }
+                        // Check for EnableContainerFromMessageExtractor call
+                        else if (methodName == "EnableContainerFromMessageExtractor")
+                        {
+                            if (IsCosmosPersistenceExtensionMethod(context, memberAccess))
+                            {
+                                hasEnableContainerFromMessageExtractor = true;
+                            }
+                        }
                     }
                 }
             }
+
+            // Report diagnostic if we have both default container and extractors but no EnableContainerFromMessageExtractor
+            if (hasDefaultContainer && hasContainerExtractor && !hasEnableContainerFromMessageExtractor)
+            {
+                var diagnostic = Diagnostic.Create(
+                    MissingEnableContainerFromMessageExtractor,
+                    body.GetLocation());
+
+                context.ReportDiagnostic(diagnostic);
+            }
         }
 
-        // Report diagnostic if we have both default container and extractors but no EnableContainerFromMessageExtractor
-        if (hasDefaultContainer && hasContainerExtractor && !hasEnableContainerFromMessageExtractor)
+        static bool IsCosmosPersistenceExtensionMethod(SyntaxNodeAnalysisContext context, MemberAccessExpressionSyntax memberAccess)
         {
-            var diagnostic = Diagnostic.Create(
-                MissingEnableContainerFromMessageExtractor,
-                body.GetLocation());
+            var semanticModel = context.SemanticModel;
+            var type = semanticModel.GetTypeInfo(memberAccess.Expression);
 
-            context.ReportDiagnostic(diagnostic);
+            return type.Type.ToString() == "NServiceBus.PersistenceExtensions<NServiceBus.CosmosPersistence>";
         }
     }
 
-    static bool IsCosmosPersistenceExtensionMethod(SyntaxNodeAnalysisContext context, MemberAccessExpressionSyntax memberAccess)
-    {
-        var semanticModel = context.SemanticModel;
-        var type = semanticModel.GetTypeInfo(memberAccess.Expression);
 
-        return type.Type.ToString() == "NServiceBus.PersistenceExtensions<NServiceBus.CosmosPersistence>";
-    }
 }
-
-
