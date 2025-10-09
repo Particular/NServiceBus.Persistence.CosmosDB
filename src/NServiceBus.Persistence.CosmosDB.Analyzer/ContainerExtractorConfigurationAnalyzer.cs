cbbd44167aa021b0c15c025ee5c5d9e77b65b22d
Jo Palac <jo.palac@particular.net>
2025-10-09 12:29:40 +1000

    remove commented out code

--- src/NServiceBus.Persistence.CosmosDB.Analyzer/ContainerExtractorConfigurationAnalyzer.cs
+++ src/NServiceBus.Persistence.CosmosDB.Analyzer/ContainerExtractorConfigurationAnalyzer.cs
@@ -17,7 +17,7 @@
             "NSBC001",
             "EnableContainerFromMessageExtractor should be called when using both default container and message extractors",
             "The endpoint has both default container and message container extractors configured, but does not have EnableContainerFromMessageExtractor set. Consider calling persistence.EnableContainerFromMessageExtractor().",
-            "Configuration",
+            "NServiceBus.Persistence.CosmosDB",
             DiagnosticSeverity.Warning,
             isEnabledByDefault: true,
             description: "When configuring both a default container and container message extractors, you should call EnableContainerFromMessageExtractor to ensure the message extractors take precedence.");
@@ -30,8 +30,6 @@
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
             context.EnableConcurrentExecution();
             context.RegisterSyntaxNodeAction(Analyze, SyntaxKind.InvocationExpression);
-            //context.RegisterSyntaxNodeAction(AnalyzeMethodDeclaration, SyntaxKind.MethodDeclaration);
-            //context.RegisterSyntaxNodeAction(AnalyzeConstructorDeclaration, SyntaxKind.ConstructorDeclaration);
         }
 
         static void Analyze(SyntaxNodeAnalysisContext context)
@@ -144,8 +142,8 @@
 
             var hasDefaultContainer = false;
             var hasEnableContainerFromMessageExtractor = false;
-
-            while (!hasDefaultContainer && !hasEnableContainerFromMessageExtractor)
+            var finished = false;
+            while (!finished && !hasDefaultContainer && !hasEnableContainerFromMessageExtractor)
             {
                 foreach (var statement in statements)
                 {
@@ -177,85 +175,11 @@
                         }
                     }
                 }
+                finished = true;
             }
 
             return hasDefaultContainer && !hasEnableContainerFromMessageExtractor;
         }
-
-        //static void AnalyzeMethodDeclaration(SyntaxNodeAnalysisContext context)
-        //{
-        //    var methodDeclaration = (MethodDeclarationSyntax)context.Node;
-        //    AnalyzeForContainerConfiguration(context, methodDeclaration.Body);
-        //}
-
-        //static void AnalyzeConstructorDeclaration(SyntaxNodeAnalysisContext context)
-        //{
-        //    var constructorDeclaration = (ConstructorDeclarationSyntax)context.Node;
-        //    AnalyzeForContainerConfiguration(context, constructorDeclaration.Body);
-        //}
-
-        //static void AnalyzeForContainerConfiguration(SyntaxNodeAnalysisContext context, BlockSyntax body)
-        //{
-        //    if (body == null)
-        //    {
-        //        return;
-        //    }
-
-        //    var statements = body.Statements;
-
-        //    var hasDefaultContainer = false;
-        //    var hasContainerExtractor = false;
-        //    var hasEnableContainerFromMessageExtractor = false;
-
-        //    foreach (var statement in statements)
-        //    {
-        //        var expressionStatements = statement.DescendantNodesAndSelf()
-        //            .OfType<InvocationExpressionSyntax>();
-
-        //        foreach (var invocation in expressionStatements)
-        //        {
-        //            if (invocation.Expression is MemberAccessExpressionSyntax memberAccess)
-        //            {
-        //                var methodName = memberAccess.Name.Identifier.ValueText;
-
-        //                // Check for DefaultContainer call on the correct type
-        //                if (methodName == "DefaultContainer")
-        //                {
-        //                    if (IsCorrectObjectMethod(context, memberAccess, cosmosPersistenceExtension))
-        //                    {
-        //                        hasDefaultContainer = true;
-        //                    }
-        //                }
-        //                // Check for container extractor methods
-        //                else if (methodName.StartsWith("ExtractContainerInformationFromMessage"))
-        //                {
-        //                    if (IsCorrectObjectMethod(context, memberAccess, transactionInformationConfiguration))
-        //                    {
-        //                        hasContainerExtractor = true;
-        //                    }
-        //                }
-        //                // Check for EnableContainerFromMessageExtractor call
-        //                else if (methodName == "EnableContainerFromMessageExtractor")
-        //                {
-        //                    if (IsCorrectObjectMethod(context, memberAccess, cosmosPersistenceExtension))
-        //                    {
-        //                        hasEnableContainerFromMessageExtractor = true;
-        //                    }
-        //                }
-        //            }
-        //        }
-        //    }
-
-        //    // Report diagnostic if we have both default container and extractors but no EnableContainerFromMessageExtractor
-        //    if (hasDefaultContainer && hasContainerExtractor && !hasEnableContainerFromMessageExtractor)
-        //    {
-        //        var diagnostic = Diagnostic.Create(
-        //            MissingEnableContainerFromMessageExtractor,
-        //            body.GetLocation());
-
-        //        context.ReportDiagnostic(diagnostic);
-        //    }
-        //}
 
         static bool IsCorrectObjectMethod(SyntaxNodeAnalysisContext context, MemberAccessExpressionSyntax memberAccess, string correctObject)
         {
