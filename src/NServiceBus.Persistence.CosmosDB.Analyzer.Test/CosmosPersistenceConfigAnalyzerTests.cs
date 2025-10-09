1b2135e3639ff153a1418cfdf66de1734a23f7de
Phil Bastian <phil.bastian@particular.net>
2025-10-09 15:58:10 +0800

    test for finding usage in extension method

--- src/NServiceBus.Persistence.CosmosDB.Analyzer.Test/CosmosPersistenceConfigAnalyzerTests.cs
+++ src/NServiceBus.Persistence.CosmosDB.Analyzer.Test/CosmosPersistenceConfigAnalyzerTests.cs
@@ -47,6 +47,52 @@ class Foo
     }
 
     [Test]
+    public Task DiagnosticIsReportedWhenNoEnableContainerFromMessageExtractorInExtension()
+    {
+        var source =
+            $@"using NServiceBus;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using NServiceBus.Persistence.CosmosDB;
+using System.Collections.Generic;
+
+class CustomContainerFromMessageExtractor : IContainerInformationFromMessagesExtractor
+{{
+    public bool TryExtract(object message, IReadOnlyDictionary<string, string> headers, out ContainerInformation? containerInformation)
+    {{
+        containerInformation = new ContainerInformation(""TestContainer"", new PartitionKeyPath(""/key""));
+        return true;
+    }}
+}}
+
+public static class GGExtensions
+{{
+    public static void ExtractContainerInformationFromMessage(this PersistenceExtensions<CosmosPersistence> persistence)
+    {{
+        [|persistence.TransactionInformation().ExtractContainerInformationFromMessage(new CustomContainerFromMessageExtractor())|];
+    }}
+}}
+
+class Foo
+{{
+    void Direct(EndpointConfiguration endpointConfiguration)
+    {{
+        var persistence = endpointConfiguration
+            .UsePersistence<CosmosPersistence>()
+            .CosmosClient(new CosmosClient(""asdf""))
+            .DatabaseName(""Database1"");
+
+        persistence.DefaultContainer(""DefaultContainer"", ""/messageId"");
+        persistence.ExtractContainerInformationFromMessage();
+    }}
+}}
+";
+
+        return Assert("NSBC001", source);
+    }
+
+    [Test]
     public Task DiagnosticIsNotReportedWhenEnableContainerFromMessageExtractor()
     {
         var source =
