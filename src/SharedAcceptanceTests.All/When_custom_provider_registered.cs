aba86c4f7f645ba9496d3b3e7ff9847a61f27198
Andreas Öhlund <andreas.ohlund@particular.net>
2026-05-18 13:52:35 +0200

    Add 'afterStart' parameter to custom provider registration for improved initialization

--- src/SharedAcceptanceTests.All/When_custom_provider_registered.cs
+++ src/SharedAcceptanceTests.All/When_custom_provider_registered.cs
@@ -18,7 +18,7 @@ public class When_custom_provider_registered : NServiceBusAcceptanceTest
         var context = await Scenario.Define<Context>()
             .WithEndpoint<EndpointWithCustomProvider>(b =>
             {
-                b.Services(services => services.AddSingleton<IProvideCosmosClient>(sp => new EndpointWithCustomProvider.CustomProvider(sp.GetRequiredService<Context>())));
+                b.Services(services => services.AddSingleton<IProvideCosmosClient>(sp => new EndpointWithCustomProvider.CustomProvider(sp.GetRequiredService<Context>())), afterStart: true);
                 b.When(session => session.SendLocal(new StartSaga1 { DataId = Guid.NewGuid() }));
             })
             .Done(c => c.SagaReceivedMessage)
